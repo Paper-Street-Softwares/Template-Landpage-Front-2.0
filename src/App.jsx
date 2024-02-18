8dcd84ffe89d6f35b88078886df59850a9b3d6bc
Edison Matos <edison.matos@live.com>
2024-02-18 09:16:41 -0300

    fix: Remove fragmento dentro de retorno do app

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="m-0 bg-blue-500">Hello, MPA with React and Tailwind.</div>
-    </>
+    <div className="m-0 bg-blue-500">Hello, MPA with React and Tailwind.</div>
   );
 }
 
