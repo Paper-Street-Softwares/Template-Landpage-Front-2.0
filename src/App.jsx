cdb74c1c4d9c2568fc6828651b0663ba655f4f24
Edison Matos <edison.matos@live.com>
2024-02-20 08:03:53 -0300

    feat: Importa arquivo de config do tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="m-0 bg-blue-500">
+    <div className="m-0 bg-blue-500 desktop1:text-white">
       Hello, MPA with React and Tailwind and Deploy on Vercel.
     </div>
   );
