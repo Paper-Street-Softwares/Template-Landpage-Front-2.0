5e02fe8a12e5f61b806a72d30a7490238cf11002
Edison Matos <edison.matos@live.com>
2024-02-27 15:05:06 -0300

    feat: Ajusta cor da fonte no componente AboutUs

--- src/components/sections/AboutUs.jsx
+++ src/components/sections/AboutUs.jsx
@@ -2,7 +2,7 @@ import SectionHeader from "../sectionElements/SectionHeader";
 
 export default function AboutUs() {
   return (
-    <div className="font-mainFont">
+    <div className="font-mainFont text-quaternary">
       <SectionHeader
         sectionHeaderTitleFirst="Sobre nós"
         sectionHeaderDescription="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
