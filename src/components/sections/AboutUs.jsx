fa924cabe395a284e987fae7ee4f5d4bb89e86e7
Gabriel <gabrielsouza65115@gmail.com>
2024-02-28 07:32:24 -0300

    feat: Insere o texto Nossa história

--- src/components/sections/AboutUs.jsx
+++ src/components/sections/AboutUs.jsx
@@ -7,7 +7,13 @@ export default function AboutUs() {
         sectionHeaderTitleFirst="Sobre nós"
         sectionHeaderDescription="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
       />
-      <div>Aqui será onde você vai inserir o restante</div>
+      <div className="container flex justify-between w-[1,110px] items-center pt-[3%]">
+        <div className="textos">
+          <h3 class=' font-medium leading-[24px] text-left'>Nossa História</h3>
+          
+        </div>
+        
+      </div>
     </div>
   );
 }
