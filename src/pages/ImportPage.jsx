8657c8790bf416d3088b076ce7234842dd324bda
Edison Matos <edison.matos@live.com>
2024-03-11 15:51:40 -0300

    feat: Ajusta tamanho das fontes do acordion

--- src/pages/ImportPage.jsx
+++ src/pages/ImportPage.jsx
@@ -28,11 +28,16 @@ export default function ImportPage() {
         <ServiceCard icon={icon} title="Título" description="Description" />
       </SectionContent>
       <MarginBetweenSection />
+      <SectionHeader
+        sectionHeaderTitleSecond="Perguntas Frequentes"
+        sectionHeaderDescription="Ficou com alguma dúvida? Clique na pergunta abaixo e obtenha a resposta. 
+        Caso não tenha encontrado a resposta que precisa, entre em contato conosco e iremos te esclarecer."
+      />
       <div className="flex">
         <div className="w-full desktop1:w-[60%]">
           <Acordion />
         </div>
-        <div className="w-full hidden desktop1:block desktop1:w-[40%] flex justify-center">
+        <div className="w-full hidden desktop1:block desktop1:w-[40%]">
           <img src={faqImg} alt="Faq Img"></img>
         </div>
       </div>
