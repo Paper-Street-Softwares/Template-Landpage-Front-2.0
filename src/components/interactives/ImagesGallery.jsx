f42e5f54fe27298492db89197904a63207f37227
Edison Matos <edison.matos@live.com>
2024-03-12 09:50:00 -0300

    feat: Insere imagens, importa no imageGalley e insere na galeria

--- src/components/interactives/ImagesGallery.jsx
+++ src/components/interactives/ImagesGallery.jsx
@@ -1,19 +1,23 @@
 import ImageGallery from "react-image-gallery";
+import slide1 from "../../assets/banners/aboutUs/slide1bigPool.jpg";
+import slide2 from "../../assets/banners/aboutUs/slide2smallPool.jpg";
+import slide3 from "../../assets/banners/aboutUs/slide3cleanWater.jpg";
+
 import "react-image-gallery/styles/css/image-gallery.css";
 
 export default function ImagesGallery() {
   const images = [
     {
-      original: "https://picsum.photos/id/504/1110/500.jpg",
-      thumbnail: "https://picsum.photos/id/504/250/150.jpg",
+      original: slide1,
+      thumbnail: slide1,
     },
     {
-      original: "https://picsum.photos/id/668/1110/500.jpg",
-      thumbnail: "https://picsum.photos/id/668/250/150.jpg",
+      original: slide2,
+      thumbnail: slide2,
     },
     {
-      original: "https://picsum.photos/id/445/1110/500.jpg",
-      thumbnail: "https://picsum.photos/id/445/250/150.jpg",
+      original: slide3,
+      thumbnail: slide3,
     },
   ];
 
