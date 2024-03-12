3da56bf01985456d8894b00d8baeffea33d54356
Edison Matos <edison.matos@live.com>
2024-03-12 11:36:28 -0300

    feat: Cria seção FAQ

--- src/pages/ImportPage.jsx
+++ src/pages/ImportPage.jsx
@@ -3,15 +3,13 @@ import SectionContent from "../components/sectionElements/SectionContent";
 import ServiceCard from "../components/cards/ServiceCard";
 import icon from "../assets/importAssets/expertise.png";
 import bgImg from "../assets/importAssets/bg.jpg";
-import faqImg from "../assets/importAssets/faq.png";
 import BlankParallaxe from "../components/sectionElements/BlankParallaxe";
-import Acordion from "../components/interactives/Acordion";
 import MarginBetweenSection from "../components/sectionElements/MarginBetweenSection";
 import AboutUs from "../components/sections/AboutUs";
 import BackToTopButton from "../components/interactives/BackToTopButton";
 import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
-import MotionDivRightToLeft from "../components/animation/MotionDivRightToLeft";
 import BannersCarousel from "../components/sections/BannersCarousel";
+import FAQ from "../components/sections/FAQ";
 
 export default function ImportPage() {
   return (
@@ -32,27 +30,13 @@ export default function ImportPage() {
         <ServiceCard icon={icon} title="Título" description="Description" />
       </SectionContent>
       <MarginBetweenSection />
-      <BlankParallaxe bgImg={bgImg} />
-      <MarginBetweenSection />
       <AboutUs />
+      <MarginBetweenSection />
+      <BlankParallaxe bgImg={bgImg} />
       <MarginBetweenSection />
       <BannersCarousel />
       <MarginBetweenSection />
-      <SectionHeader
-        sectionHeaderTitleSecond="Perguntas Frequentes"
-        sectionHeaderDescription="Ficou com alguma dúvida? Clique na pergunta abaixo e obtenha a resposta. 
-        Caso não tenha encontrado a resposta que precisa, entre em contato conosco e iremos te esclarecer."
-      />
-      <div className="flex justify-between ">
-        <div className="w-full desktop1:w-[60%] desktop2:w-[63%]">
-          <Acordion />
-        </div>
-        <div className="hidden desktop1:block">
-          <MotionDivRightToLeft>
-            <img src={faqImg} alt="Faq Img"></img>
-          </MotionDivRightToLeft>
-        </div>
-      </div>
+      <FAQ />
       <BackToTopButton />
       <FloatingWhatsappButton />
       <MarginBetweenSection />
