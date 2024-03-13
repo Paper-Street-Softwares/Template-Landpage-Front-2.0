9b95b8465259b08d2affb3c38be62fd653238517
Edison Matos <edison.matos@live.com>
2024-03-13 10:34:52 -0300

    feat: Implementa hover e animação

--- src/components/cards/ImgAndButtonsCard.jsx
+++ src/components/cards/ImgAndButtonsCard.jsx
@@ -18,13 +18,13 @@ export default function ImgAndButtonsCard(props) {
       </div>
 
       <a href={button1Link} target="_blank" className="w-full">
-        <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full">
+        <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full transition hover:bg-red-900">
           {button1Label}
         </button>
       </a>
 
       <a href={button2Link} target="_blank" className="w-full">
-        <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full">
+        <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full hover:bg-red-900 transition">
           {button2Label}
         </button>
       </a>
--- src/components/sections/OurProducts.jsx
+++ src/components/sections/OurProducts.jsx
@@ -1,8 +1,14 @@
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionContent from "../sectionElements/SectionContent";
+import ImgAndButtonsCard from "../cards/ImgAndButtonsCard";
 import hthLogo from "../../assets/imgs/brands/hthLogo.png";
 import gencoLogo from "../../assets/imgs/brands/gencoLogo.png";
-import ImgAndButtonsCard from "../cards/ImgAndButtonsCard";
+import domclorLogo from "../../assets/imgs/brands/domclorLogo.png";
+import hidroazulLogo from "../../assets/imgs/brands/hidroazulLogo.png";
+import neoclorLogo from "../../assets/imgs/brands/neoclorLogo.png";
+import netunoLogo from "../../assets/imgs/brands/netunoLogo.png";
+import sodramarLogo from "../../assets/imgs/brands/sodramarLogo.png";
+import suallLogo from "../../assets/imgs/brands/suallLogo.png";
 
 export default function OurProducts() {
   return (
@@ -28,45 +34,45 @@ export default function OurProducts() {
             button2Link="#"
           />
           <ImgAndButtonsCard
-            img={hthLogo}
+            img={neoclorLogo}
             button1Label="Cloros"
             button1Link="#"
             button2Label="Auxiliares"
             button2Link="#"
           />
           <ImgAndButtonsCard
-            img={hthLogo}
+            img={hidroazulLogo}
             button1Label="Cloros"
             button1Link="#"
             button2Label="Auxiliares"
             button2Link="#"
           />
           <ImgAndButtonsCard
-            img={hthLogo}
+            img={domclorLogo}
             button1Label="Cloros"
             button1Link="#"
             button2Label="Auxiliares"
             button2Link="#"
           />
           <ImgAndButtonsCard
-            img={hthLogo}
+            img={suallLogo}
             button1Label="Cloros"
             button1Link="#"
             button2Label="Auxiliares"
             button2Link="#"
           />
           <ImgAndButtonsCard
-            img={hthLogo}
-            button1Label="Cloros"
+            img={sodramarLogo}
+            button1Label="Limpeza"
             button1Link="#"
-            button2Label="Auxiliares"
+            button2Label="Construção"
             button2Link="#"
           />
           <ImgAndButtonsCard
-            img={hthLogo}
-            button1Label="Cloros"
+            img={netunoLogo}
+            button1Label="Limpeza"
             button1Link="#"
-            button2Label="Auxiliares"
+            button2Label="Construção"
             button2Link="#"
           />
         </SectionContent>
