76226b7b28861a707ce39dc3620641ea3dbea12e
Edison Matos <edison.matos@live.com>
2024-03-13 11:41:15 -0300

    feat: Insere links dos cursos

--- src/components/cards/ImgTextAndButtonsCard.jsx
+++ src/components/cards/ImgTextAndButtonsCard.jsx
@@ -1,31 +1,26 @@
 import PropTypes from "prop-types";
 
-export default function ImgAndButtonsCard(props) {
-  const { img, button1Label, button1Link, button2Label, button2Link } = props;
+export default function ImgTextAndButtonsCard(props) {
+  const { img, button1Label, button1Link, text } = props;
 
-  ImgAndButtonsCard.propTypes = {
+  ImgTextAndButtonsCard.propTypes = {
     img: PropTypes.any,
     button1Label: PropTypes.any,
     button1Link: PropTypes.any,
-    button2Label: PropTypes.any,
-    button2Link: PropTypes.any,
+    text: PropTypes.any,
   };
 
   return (
-    <div className="w-full phone3:w-[45%] desktop1:w-[23%]  bg-white p-[15px] my-[14px] rounded-[10px] flex flex-col gap-[15px]">
+    <div className="w-full tablet1:w-[45%] desktop1:w-[29%]  bg-white p-[15px] my-[14px] rounded-[10px] flex flex-col gap-[15px]">
       <div className="flex justify-center h-[125px] phone3:h-[80px] tablet1:h-[125px]">
         <img src={img} alt="hthLogo" />
       </div>
-
+      <p className="text-center font-mainFont text-paragraph3 text-quaternary">
+        {text}
+      </p>
       <a href={button1Link} target="_blank" className="w-full">
         <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full transition hover:bg-red-900">
           {button1Label}
-        </button>
-      </a>
-
-      <a href={button2Link} target="_blank" className="w-full">
-        <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full hover:bg-red-900 transition">
-          {button2Label}
         </button>
       </a>
     </div>
--- src/components/sections/Courses.jsx
+++ src/components/sections/Courses.jsx
@@ -1,8 +1,9 @@
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionContent from "../sectionElements/SectionContent";
 import hthLogo from "../../assets/imgs/brands/hthLogo.png";
-import gencoLogo from "../../assets/imgs/brands/gencoLogo.png";
+import gencoLogo from "../../assets/imgs/brands/genco2Logo.png";
 import hidroazulLogo from "../../assets/imgs/brands/hidroazulLogo.png";
+import ImgTextAndButtonsCard from "../cards/ImgTextAndButtonsCard";
 
 export default function Courses() {
   return (
@@ -13,7 +14,24 @@ export default function Courses() {
           sectionHeaderDescription="Nesta seção, você será direcionado para agendar o curso ou palestra de forma espontânea, de acordo com seu interesse no aprendizado que o assunto desperta. Nosso objetivo é que você se sinta livre para escolher entre diferentes fabricantes, sem sentir que está sendo direcionado de forma alguma. Estamos aqui para apoiá-lo em sua jornada de conhecimento."
         />
         <SectionContent>
-          <di>a</di>
+          <ImgTextAndButtonsCard
+            img={hthLogo}
+            text="Curso presencial de tratamento de águas de piscinas."
+            button1Label="Visitar site"
+            button1Link="https://hth.com.br/cursos-online/entrar"
+          />
+          <ImgTextAndButtonsCard
+            img={gencoLogo}
+            text="Curso online de tratamento de águas de piscinas."
+            button1Label="Visitar site"
+            button1Link="https://www.genco.com.br/escola-da-piscina/"
+          />
+          <ImgTextAndButtonsCard
+            img={hidroazulLogo}
+            text="Curso completo na Universidade da Piscina."
+            button1Label="Visitar site"
+            button1Link="https://hidroazul.com.br/universidade-da-piscina/"
+          />
         </SectionContent>
       </div>
     </div>
