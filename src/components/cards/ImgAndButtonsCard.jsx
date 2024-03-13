8657019bf859d4309b2ecf19485fb2b19344cd98
Edison Matos <edison.matos@live.com>
2024-03-13 12:03:06 -0300

    feat: Implementa animações nos cards

--- src/components/cards/ImgAndButtonsCard.jsx
+++ src/components/cards/ImgAndButtonsCard.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
+import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
 
 export default function ImgAndButtonsCard(props) {
-  const { img, button1Label, button1Link, button2Label, button2Link } = props;
+  const { img, button1Label, button1Link, button2Label, button2Link, a } =
+    props;
 
   ImgAndButtonsCard.propTypes = {
     img: PropTypes.any,
@@ -9,23 +11,26 @@ export default function ImgAndButtonsCard(props) {
     button1Link: PropTypes.any,
     button2Label: PropTypes.any,
     button2Link: PropTypes.any,
+    a: PropTypes.any,
   };
 
   return (
     <div className="w-full phone3:w-[45%] desktop1:w-[23%]  bg-white p-[15px] my-[14px] rounded-[10px] flex flex-col gap-[15px]">
       <div className="flex justify-center h-[125px] phone3:h-[80px] tablet1:h-[125px]">
-        <img src={img} alt="hthLogo" />
+        <MotionDivLeftToRight>
+          <img src={img} alt="hthLogo" />
+        </MotionDivLeftToRight>
       </div>
 
       <a href={button1Link} target="_blank" className="w-full">
         <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full transition hover:bg-red-900">
-          {button1Label}
+          <MotionDivLeftToRight>{button1Label}</MotionDivLeftToRight>
         </button>
       </a>
 
       <a href={button2Link} target="_blank" className="w-full">
         <button className="text-white bg-red-700 rounded-[10px] py-[8px] px-[31px] font-mainFont font-bold w-full hover:bg-red-900 transition">
-          {button2Label}
+          <MotionDivLeftToRight>{button2Label}</MotionDivLeftToRight>
         </button>
       </a>
     </div>
