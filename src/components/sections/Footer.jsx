6e4f27c535075fe07967b468eae6236799393165
Gabriel <gabrielsouza65115@gmail.com>
2024-03-15 11:32:16 -0300

    fix: Corrige a palavra vc para você

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="full bg-secondary text-white font-mainFont text-center text-paragraph3 flex flex-col py-[28px] justify-between">
       <div className="content flex flex-col justify-between h-[100%] ">
         <p className="my-[5%] tablet1:my-[3%] tablet2:my-[2%]">
-        Na sua piscina fazemos tudo, vc nada!
+        Na sua piscina fazemos tudo, você nada!
         </p>
         <p className="my-[5%] tablet1:my-[3%] desktop3:my-[2%]">
           2024 - MPA Piscinas - Todos os direitos reservados - Website
