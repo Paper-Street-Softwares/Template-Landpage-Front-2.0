bc02e84f21aa95c4e2c095d1a747d856c072bc40
Edison Matos <edison.matos@live.com>
2024-03-16 10:04:08 -0300

    feat: Finaliza card de produto

--- src/components/sections/OurProducts.jsx
+++ src/components/sections/OurProducts.jsx
@@ -1,6 +1,7 @@
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionContent from "../sectionElements/SectionContent";
-import ImgCodeDescButtonCard from "../cards/imgCodeDescButtonCard";
+import ImgCodeDescButtonCard from "../cards/ImgCodeDescButtonCard";
+import imgProductExample from "../../assets/imgs/products/hth/cloros/baldeCloro.png";
 // import ImgAndButtonsCard from "../cards/ImgAndButtonsCard";
 // import hthLogo from "../../assets/imgs/brands/hthLogo.png";
 // import gencoLogo from "../../assets/imgs/brands/gencoLogo.png";
@@ -20,7 +21,12 @@ export default function OurProducts() {
           sectionHeaderDescription="Ao longo dos anos, fomos firmando parcerias hoje bastante consolidadas com marcas renomadas e de grande credibilidade no mercado. Encontre a marca que você deseja comprar e clique na categoria para ser levado para a página contendo os produtos desta marca."
         />
         <SectionContent>
-          <ImgCodeDescButtonCard />
+          <ImgCodeDescButtonCard
+            img={imgProductExample}
+            code="102030"
+            description="Descrição do produto descrição do produto descrição do produto descrição."
+            button1Label="Baixar Ficha FISPQ"
+          />
           {/* 
           <ImgAndButtonsCard
             img={hthLogo}
