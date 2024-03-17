beb46b9f5767e4028fe57ca6d8391cebb77e3d33
Edison Matos <edison.matos@live.com>
2024-03-17 13:03:31 -0300

    chore: Insere mais cards pra exemplo visual

--- src/components/sections/ProductsList.jsx
+++ src/components/sections/ProductsList.jsx
@@ -3,6 +3,7 @@ import SectionHeader from "../sectionElements/SectionHeader";
 import SectionContent from "../sectionElements/SectionContent";
 import ImgCodeDescButtonCard from "../cards/ImgCodeDescButtonCard";
 import imgProductExample from "../../assets/imgs/products/hth/cloros/baldeCloro.png";
+import imgProductExample2 from "../../assets/imgs/products/hth/cloros/auxiliares.png";
 
 export default function ProductsList() {
   const [selectedCategory, setSelectedCategory] = useState("all");
@@ -43,9 +44,29 @@ export default function ProductsList() {
               category="cloros"
             />
           ) : null}
-          {selectedCategory === "auxiliares" || selectedCategory === "all" ? (
+          {selectedCategory === "cloros" || selectedCategory === "all" ? (
             <ImgCodeDescButtonCard
               img={imgProductExample}
+              code="Esse é cloro"
+              description="Descrição do produto descrição do produto descrição do produto descrição."
+              button1Label="Baixar Ficha FISPQ"
+              button1Link="#"
+              category="cloros"
+            />
+          ) : null}
+          {selectedCategory === "auxiliares" || selectedCategory === "all" ? (
+            <ImgCodeDescButtonCard
+              img={imgProductExample2}
+              code="Auxiliar"
+              description="Descrição do produto descrição do produto descrição do produto descrição."
+              button1Label="Baixar Ficha FISPQ"
+              button1Link="#"
+              category="auxiliares"
+            />
+          ) : null}
+          {selectedCategory === "auxiliares" || selectedCategory === "all" ? (
+            <ImgCodeDescButtonCard
+              img={imgProductExample2}
               code="Auxiliar"
               description="Descrição do produto descrição do produto descrição do produto descrição."
               button1Label="Baixar Ficha FISPQ"
