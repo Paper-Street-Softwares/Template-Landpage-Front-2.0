c1dd797f3bf4561e56ebd9108350bfecc10adee1
Edison Matos <edison.matos@live.com>
2024-03-17 12:55:05 -0300

    feat: Criado cenário de exemplo pra sistema de filtro por categoria

--- src/components/sections/ProductsList.jsx
+++ src/components/sections/ProductsList.jsx
@@ -11,14 +11,25 @@ export default function ProductsList() {
           sectionHeaderTitleSecond="Linha de Produtos"
           sectionHeaderDescription="Ao longo dos anos, fomos firmando parcerias hoje bastante consolidadas com marcas renomadas e de grande credibilidade no mercado. Encontre a marca que você deseja comprar e clique na categoria para ser levado para a página contendo os produtos desta marca."
         />
+        <button className="bg-red-900 hover:bg-red-700">Cloros</button>
+        <button className="bg-blue-900 hover:bg-blue-700">Auxiliares</button>
+        <button className="bg-green-900 hover:bg-green-700">Todos</button>
         <SectionContent>
           <ImgCodeDescButtonCard
             img={imgProductExample}
-            code="102030"
+            code="Esse é cloro"
             description="Descrição do produto descrição do produto descrição do produto descrição."
             button1Label="Baixar Ficha FISPQ"
             button1Link="#"
             category="cloros"
+          />
+          <ImgCodeDescButtonCard
+            img={imgProductExample}
+            code="Auxiliar"
+            description="Descrição do produto descrição do produto descrição do produto descrição."
+            button1Label="Baixar Ficha FISPQ"
+            button1Link="#"
+            category="auxiliares"
           />
         </SectionContent>
       </div>
