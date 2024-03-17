a6c55f2e8f5c052058537298ff3bbb97b67e4f38
Edison Matos <edison.matos@live.com>
2024-03-17 12:56:12 -0300

    feat: Finaliza sistema de filtro

--- src/components/sections/ProductsList.jsx
+++ src/components/sections/ProductsList.jsx
@@ -1,9 +1,12 @@
+import React, { useState } from "react";
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionContent from "../sectionElements/SectionContent";
 import ImgCodeDescButtonCard from "../cards/ImgCodeDescButtonCard";
 import imgProductExample from "../../assets/imgs/products/hth/cloros/baldeCloro.png";
 
 export default function ProductsList() {
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
   return (
     <div className="full">
       <div className="bg-quinary content py-[26px] phone3:py-[48px]">
@@ -11,26 +14,45 @@ export default function ProductsList() {
           sectionHeaderTitleSecond="Linha de Produtos"
           sectionHeaderDescription="Ao longo dos anos, fomos firmando parcerias hoje bastante consolidadas com marcas renomadas e de grande credibilidade no mercado. Encontre a marca que você deseja comprar e clique na categoria para ser levado para a página contendo os produtos desta marca."
         />
-        <button className="bg-red-900 hover:bg-red-700">Cloros</button>
-        <button className="bg-blue-900 hover:bg-blue-700">Auxiliares</button>
-        <button className="bg-green-900 hover:bg-green-700">Todos</button>
+        <button
+          className="bg-red-900 hover:bg-red-700"
+          onClick={() => setSelectedCategory("cloros")}
+        >
+          Cloros
+        </button>
+        <button
+          className="bg-blue-900 hover:bg-blue-700"
+          onClick={() => setSelectedCategory("auxiliares")}
+        >
+          Auxiliares
+        </button>
+        <button
+          className="bg-green-900 hover:bg-green-700"
+          onClick={() => setSelectedCategory("all")}
+        >
+          Todos
+        </button>
         <SectionContent>
-          <ImgCodeDescButtonCard
-            img={imgProductExample}
-            code="Esse é cloro"
-            description="Descrição do produto descrição do produto descrição do produto descrição."
-            button1Label="Baixar Ficha FISPQ"
-            button1Link="#"
-            category="cloros"
-          />
-          <ImgCodeDescButtonCard
-            img={imgProductExample}
-            code="Auxiliar"
-            description="Descrição do produto descrição do produto descrição do produto descrição."
-            button1Label="Baixar Ficha FISPQ"
-            button1Link="#"
-            category="auxiliares"
-          />
+          {selectedCategory === "cloros" || selectedCategory === "all" ? (
+            <ImgCodeDescButtonCard
+              img={imgProductExample}
+              code="Esse é cloro"
+              description="Descrição do produto descrição do produto descrição do produto descrição."
+              button1Label="Baixar Ficha FISPQ"
+              button1Link="#"
+              category="cloros"
+            />
+          ) : null}
+          {selectedCategory === "auxiliares" || selectedCategory === "all" ? (
+            <ImgCodeDescButtonCard
+              img={imgProductExample}
+              code="Auxiliar"
+              description="Descrição do produto descrição do produto descrição do produto descrição."
+              button1Label="Baixar Ficha FISPQ"
+              button1Link="#"
+              category="auxiliares"
+            />
+          ) : null}
         </SectionContent>
       </div>
     </div>
