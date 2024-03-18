7d209f094a1173fbe927ada51195d05226dd72f4
Edison Matos <edison.matos@live.com>
2024-03-18 10:43:06 -0300

    fix: Melhora tamanho de fonte da seção cta whatsapp

--- src/components/sections/CtaWhatsapp.jsx
+++ src/components/sections/CtaWhatsapp.jsx
@@ -21,7 +21,7 @@ export default function CtaWhatsapp() {
         <div className="w-full">
           <div className="flex flex-row items-center justify-between h-28 phone1:h-40 phone3:h-48 desktop1:h-60">
             <div className="flex flex-col w-full justify-evenly items-left h-28 phone1:h-40 phone3:h-48 desktop1:h-52">
-              <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 phone3:max-w-none font-poppins tablet1:text-title2 tablet2:text-title3 desktop1:text-title5">
+              <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 phone3:max-w-none font-poppins tablet1:text-title2 tablet2:text-title3 desktop1:text-title5 desktop2:text-title6">
                 <MotionDivLeftToRight>
                   Contate-nos através do <strong>Whatsapp</strong>
                 </MotionDivLeftToRight>
