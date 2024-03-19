b1e18d419a0f3cdd969d0db916fdcb981c9e9558
Gabriel <adans.gabriel@gmail.com>
2024-03-19 07:34:23 -0300

    feat: Engloba elementos da Hero em div que os dispõe da forma correta fazendo com que as margens não excedam o limite da Hero

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -10,44 +10,46 @@ export default function HeroSection() {
         className="w-full bg-center bg-cover content min-h-[518px]"
         style={{ backgroundImage: "url(" + bgImage + ")" }}
       >
-        <div className="w-full h-auto text-white">
-          <div className="flex flex-col items-center justify-center content phone1:mt-40 phone1:h-20 phone3:mt-40">
-            <MotionDivLeftToRight>
-              <h1 className="font-black leading-none w-[60%] phone1:text-title4 phone1:w-full text-center phone3:text-title5 tablet1:text-title6">
-                BEM VINDO AO MUNDO DAS PISCINAS!
-              </h1>
-              <p className="w-full mt-4 text-center phone1:w-full phone1:font-medium phone1:text-paragraph3 phone3:text-paragraph4 tablet1:text-title1">
-                Aqui você será atendido por profissionais qualificados que
-                prestarão toda orientação que você precisa. Continue navegando e
-                encontre o que você precisa!
-              </p>
-            </MotionDivLeftToRight>
-          </div>
-          <div className="w-full mb-10 -mt-24 tablet2:w-auto">
-            <MotionDivRightToLeft>
-              <div className="flex justify-center w-full phone1:mt-32 phone3:mt-40 phone1:w-full">
-                <a
-                  href="https://wa.me/55"
-                  className="flex flex-row items-center justify-around w-64 h-16 transition rounded-lg bg-primary hover:bg-secondary"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <div className="flex">
-                    <img
-                      src={WhatsAppIcon}
-                      className="h-20 ml-4"
-                      alt="WhatsApp Icon"
-                    />
+        <div className="flex items-center justify-center">
+          <div className="w-full h-auto text-white">
+            <div className="flex flex-col items-center justify-center content phone1:mt-40 phone1:h-20 phone3:mt-40">
+              <MotionDivLeftToRight>
+                <h1 className="font-black leading-none w-[60%] phone1:text-title4 phone1:w-full text-center phone3:text-title5 tablet1:text-title6">
+                  BEM VINDO AO MUNDO DAS PISCINAS!
+                </h1>
+                <p className="w-full mt-4 text-center phone1:w-full phone1:font-medium phone1:text-paragraph3 phone3:text-paragraph4 tablet1:text-title1">
+                  Aqui você será atendido por profissionais qualificados que
+                  prestarão toda orientação que você precisa. Continue navegando e
+                  encontre o que você precisa!
+                </p>
+              </MotionDivLeftToRight>
+            </div>
+            <div className="w-full mb-10 -mt-24 tablet2:w-auto">
+              <MotionDivRightToLeft>
+                <div className="flex justify-center w-full phone1:mt-32 phone3:mt-40 phone1:w-full">
+                  <a
+                    href="https://wa.me/55"
+                    className="flex flex-row items-center justify-around w-64 h-16 transition rounded-lg bg-primary hover:bg-secondary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <div className="flex">
+                      <img
+                        src={WhatsAppIcon}
+                        className="h-20 ml-4"
+                        alt="WhatsApp Icon"
+                      />
 
-                    <div className="flex items-center ml-6">
-                      <p className="leading-none text-white text-title1">
-                        Falar com atendente
-                      </p>
+                      <div className="flex items-center ml-6">
+                        <p className="leading-none text-white text-title1">
+                          Falar com atendente
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </a>
-              </div>
-            </MotionDivRightToLeft>
+                  </a>
+                </div>
+              </MotionDivRightToLeft>
+            </div>
           </div>
         </div>
       </div>
