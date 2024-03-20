a2c50aaada917688fe67e7e2dd94a042d1f33fe8
Gabriel <gabrielsouza65115@gmail.com>
2024-03-19 21:56:19 -0300

    feat: Finaliza a estilizaçao nas telas de tablet1 até desktop3

--- src/components/sectionElements/SectionArticle.jsx
+++ src/components/sectionElements/SectionArticle.jsx
@@ -1,19 +1,23 @@
 import PropTypes from "prop-types";
 
 function Article(props) {
+	
 	const articleTitle = props.articleTitle;
 	const articleSubtitle = props.articleSubtitle;
 	const articleParagraphfirst = props.articleParagraphfirst;
 	const articleParagraphTwo = props.articleParagraphTwo;
+	const articleParagraphThree = props.articleParagraphThree;
     const articleImage = props.articleImage;
     
 	
 
 	Article.propTypes = {
+		
 		articleTitle: PropTypes.any,
 		articleSubtitle: PropTypes.any,
 		articleParagraphfirst: PropTypes.any,
 		articleParagraphTwo: PropTypes.any,
+		articleParagraphThree: PropTypes.any,
 		articleImage: PropTypes.any,
         
 		
@@ -22,12 +26,13 @@ function Article(props) {
 	
 	return (
 		<div className= "flex w-100% justify-between items-center flex-col desktop1:flex-row phone1:pt-[5%] ">
-			<div className="  w-[100%] gap-[10%] desktop1:w-[32%] desktop2:w-[47%] desktop3:w-[60%]">
-				<h3 className='pb-[3%] text-quaternary font-medium text-paragraph3 leading-[24%] phone3:pb-[2%] phone3:text-paragraph4 phone3:leading-[27%] desktop1:pb-[3%]'>{articleSubtitle}</h3>
-				<h1 className='ont-medium text-title2 leading-[150%] text-primary phone3:text-title5 tablet1:font-medium tablet1:text-title4'>{articleTitle}</h1>
-				<p className=' text-quaternary font-normal text-paragraph3 leading-[150%] phone3:text-paragraph4 tablet1:text-paragraph3 desktop1:text-paragraph2 desktop2:text-paragraph3 desktop3:size-[79%]'>{articleParagraphfirst}</p>
-				<p className=" flex pt-[5%] text-quaternary font-normal text-paragraph3 leading-[150%] phone3:text-paragraph4 tablet1:text-paragraph3 desktop1:text-paragraph2 desktop2:text-paragraph3 desktop3:size-[79%] ">{articleParagraphTwo}</p>
-                
+			<div className="  w-[100%] gap-[5%] desktop1:w-[510%] desktop2:w-[100%] desktop3:w-[100%]">
+				<h3 className='pb-[3%] text-quaternary font-medium text-paragraph3 leading-[24%] phone3:pb-[2%] phone3:text-paragraph4 phone3:leading-[27%] desktop1:text-paragraph4 desktop1:pb-[3%]'>{articleSubtitle}</h3>
+				<h1 className='font-medium text-title2 leading-[150%] text-primary tablet2:mb-[-2%] desktop1:text-title4 desktop1:size-[130%] '>{articleTitle}</h1>
+				<p className=' phone1:pt-[5%] text-quaternary font-normal text-paragraph3 leading-[150%] desktop2:size-[120%]'>{articleParagraphfirst}</p>
+				<p className=" flex pt-[4%] text-quaternary font-normal text-paragraph3 leading-[150%] desktop2:size-[130%] ">{articleParagraphTwo}</p>
+                <p className=" flex pt-[4%] text-quaternary font-normal text-paragraph3 leading-[150%] desktop2:size-[130%] ">{articleParagraphThree}</p>
+
 			</div>
 			<div className="flex justify-center mt-[8%] w-[103.7%] phone2:w-[100.5%] phone3:w-[100.2%] phone3:mt-[5%] tablet1:size-[100.1%] tablet1:mt-[4%] tablet2:mt-[4%] desktop1:w-[43.5%] desktop3:size-[60%] desktop3:w-[50%] desktop3:mb-[3%]">{articleImage}
 				 
--- src/components/sections/AvoidAcidents.jsx
+++ src/components/sections/AvoidAcidents.jsx
@@ -1,6 +1,8 @@
 import SectionHeader from "../sectionElements/SectionHeader";
 import imgPastilhaCloro from "../../assets/imgs/avoidAccidents/pastilhaCloro.jpg";
+import ResgateAcidente from "../../assets/imgs/avoidAccidents/ResgateAcidente.jpg";
 import Article from "../sectionElements/SectionArticle";
+import ArticleComponent from "../sectionElements/ArticleComponent";
 
 export default function AvoidAccidents() {
   return (
@@ -9,26 +11,39 @@ export default function AvoidAccidents() {
         sectionHeaderTitleSecond="Evite acidentes"
         sectionHeaderDescription="Nesta seção, abordaremos as práticas de segurança essenciais para o armazenamento e manuseio correto de produtos para sua piscina."
       />
-      <div>
-      <div>
-        <img className="pb-[3%] w-full" src={imgPastilhaCloro}/>
-        <Article
-          articleSubtitle="Atenção"
-          articleTitle="Cuidados com o manuseio dos produtos"
-          articleParagraphfirst="Os produtos que você vai utilizar na sua piscina devem ser armazenados de forma segura, longe do alcance de crianças e em lugar seco e ventilado."
-          articleParagraphTwo="Guarde sempre os produtos separadamente. Existem produtos que não são compatíveis uns com os outros e, se misturados, podem gerar gases nocivos à saúde e até incêndio, já que alguns são inflamáveis."
-        />
-          <div className="mt-[-10%]">
+
+      <ArticleComponent>
+        <div className="desktop1:flex desktop1:flex-row-reverse ">
+          <div className="desktop1:w-[86.2%]">
+            <img className="w-full desktop1:pt-[4%]" src={imgPastilhaCloro} />
+          </div>
+          <div className="desktop1:w-[100%]">
             <Article
-              articleParagraphfirst="Nunca reutilize embalagens vazias, mesmo depois de serem lavadas. Elas podem acabar sendo usadas por pessoas desavisadas, crianças e até animais, e isso pode causar grande desconforto e danos à saúde."
+              articleSubtitle="Atenção"
+              articleTitle="Cuidados com o manuseio dos produtos"
+              articleParagraphfirst="Os produtos que você vai utilizar na sua piscina devem ser armazenados de forma segura, longe do alcance de crianças e em lugar seco e ventilado"
+              articleParagraphTwo="Guarde sempre os produtos separadamente. Existem produtos que não são compatíveis uns com os outros e, se misturados, podem gerar gases nocivos à saúde e até incêndio, já que alguns são inflamáveis."
+              articleParagraphThree="Nunca reutilize embalagens vazias, mesmo depois de serem lavadas. Elas podem acabar sendo usadas por pessoas desavisadas, crianças e até animais, e isso pode causar grande desconforto e danos à saúde."
             />
           </div>
-      </div>
-      </div>
-      
-      
-      
-      
+        </div>
+      </ArticleComponent>
+      <ArticleComponent>
+        <div className="desktop1:flex desktop1:flex-row desktop1:gap-[5%] desktop1:w-[100%]">
+          <div className="desktop1:w-[100%]">
+            <img
+              className="phone3:mb-[-5%] rounded-[5%] w-full"
+              src={ResgateAcidente}
+            />
+          </div>
+          <div className="desktop1:w-[120%] desktop1:relative desktop1:left-[4.5%] desktop1:mt-[-8%] desktop2:mt-[-6.5%] desktop2:w-[115%]">
+            <Article
+              articleParagraphTwo="Utilize sempre um balde plástico pequeno para fazer a dissolução do cloro ou de outro produto em pó, como barrilha, sulfato de alumínio, elevador de alcalinidade, entre outros. Esse balde deverá sempre estar guardado em lugar seguro e não poderá ser utilizado para outros fins."
+              articleParagraphThree="Tomando todos esses cuidados e outros relacionados à segurança da sua piscina, você garante uma diversão alegre e segura para todos os usuários, inclusive crianças e idosos. Boa diversão para todos!"
+            />
+          </div>
+        </div>
+      </ArticleComponent>
     </div>
   );
 }
