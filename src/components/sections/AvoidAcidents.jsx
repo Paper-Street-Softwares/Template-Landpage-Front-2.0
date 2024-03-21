0cdd7ffe65f00a713472fd35ddccd83633333f3c
Edison Matos <edison.matos@live.com>
2024-03-21 17:00:19 -0300

    feat: Aplica propriedade hidden ao articleSubtitle em AvoidAcidents

--- src/components/sections/AvoidAcidents.jsx
+++ src/components/sections/AvoidAcidents.jsx
@@ -10,7 +10,7 @@ export default function AvoidAccidents() {
       />
       <div className="flex w-100% justify-between items-center flex-col desktop1:flex-row phone1:pt-[5%] ">
         <div className=" w-[100%] gap-[10%] desktop1:w-[32%] desktop2:w-[47%] desktop3:w-[60%]">
-          <h3 className="pb-[3%] text-quaternary font-medium text-paragraph3 leading-[24%] phone3:pb-[2%] phone3:text-paragraph4 phone3:leading-[27%] desktop1:pb-[3%] ">
+          <h3 className="hidden pb-[3%] text-quaternary font-medium text-paragraph3 leading-[24%] phone3:pb-[2%] phone3:text-paragraph4 phone3:leading-[27%] desktop1:pb-[3%] ">
             Atenção
           </h3>
           <h1 className="font-medium text-title2 leading-[150%] text-primary phone3:text-title5 tablet1:font-medium tablet1:text-title4">
