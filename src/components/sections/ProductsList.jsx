c1080401eb2703a17821a696d70e7a90205797b0
Edison Matos <edison.matos@live.com>
2024-03-21 14:27:34 -0300

    feat: Ajusta o checkbox pra desktop

--- src/components/sections/ProductsList.jsx
+++ src/components/sections/ProductsList.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
 import ImgCodeDescButtonCard from "../cards/ImgCodeDescButtonCard";
 import imgProductExample2 from "../../assets/imgs/products/hth/cloros/auxiliares.png";
 import HthProducts from "../util/productsData/hth/HthProducts";
@@ -14,40 +16,37 @@ export default function ProductsList() {
     <div className="full bg-quinary">
       <div className="content py-[26px] phone3:py-[48px] flex flex-row">
         <div className="flex flex-col items-start justify-between desktop1:flex-row font-mainFont">
-          <div className="bg-white w-full desktop1:w-[18%] mb-[32px] flex justify-around desktop1:flex-col p-2 phone3:p-4 rounded-[10px]">
-            <h3 className="p-2 font-semibold w-[30%] phone3:w-[40%] text-paragraph2 phone3:text-paragraph3 tablet1:text-paragraph5 desktop1:text-left desktop1:text-title1 text-secondary desktop1:w-auto">
+          <div className="bg-white w-full desktop1:w-[18%] mb-[32px] flex flex-col p-2 phone3:p-4 rounded-[10px]">
+            <h3 className="desktop1:mb-[10px] font-semibold w-[30%] phone3:w-[40%] text-paragraph2 phone3:text-paragraph3 tablet1:text-paragraph5 desktop1:text-left desktop1:text-title1 text-secondary desktop1:w-auto">
               Filtrar por categoria
             </h3>
-            <label className="checkbox-label text-paragraph2 phone3:text-paragraph3 w-[22%] flex flex-col text-center justify-evenly tablet1:text-paragraph5 desktop1:text-paragraph4 tablet1:flex-row desktop1:p-2">
-              <input
-                type="checkbox"
-                checked={selectedCategory === "all"}
-                onChange={() => handleCheckboxChange("all")}
-              />
-              <p className="flex items-center justify-center h-auto leading-none desktop1:ml-2">
-                Todos
-              </p>
-            </label>
-            <label className="checkbox-label text-paragraph2 phone3:text-paragraph3 w-[22%] flex flex-col text-center justify-evenly tablet1:text-paragraph5 desktop1:text-paragraph4 tablet1:flex-row desktop1:p-2">
-              <input
-                type="checkbox"
-                checked={selectedCategory === "cloros"}
-                onChange={() => handleCheckboxChange("cloros")}
-              />
-              <p className="flex items-center justify-center h-auto leading-none desktop1:ml-2">
-                Cloros
-              </p>
-            </label>
-            <label className="checkbox-label text-paragraph2 phone3:text-paragraph3 w-[22%] flex flex-col text-center justify-evenly tablet1:text-paragraph5 desktop1:text-paragraph4 tablet1:flex-row desktop1:p-2">
-              <input
-                type="checkbox"
-                checked={selectedCategory === "auxiliares"}
-                onChange={() => handleCheckboxChange("auxiliares")}
-              />
-              <p className="flex items-center justify-center h-auto leading-none desktop1:ml-2">
-                Auxiliares
-              </p>
-            </label>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={selectedCategory === "all"}
+                  onChange={() => handleCheckboxChange("all")}
+                />
+              }
+              label="Todos"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={selectedCategory === "cloros"}
+                  onChange={() => handleCheckboxChange("cloros")}
+                />
+              }
+              label="Cloros"
+            />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={selectedCategory === "auxiliares"}
+                  onChange={() => handleCheckboxChange("auxiliares")}
+                />
+              }
+              label="Auxiliares"
+            />
           </div>
           <div className="flex flex-wrap items-start justify-between w-full gap-7 desktop1:w-[76%]">
             {(selectedCategory === "cloros" || selectedCategory === "all") && (
