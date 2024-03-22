ea71ed6d21f336ebb772aec08961413c1bf481d1
Gabriel <adans.gabriel@gmail.com>
2024-03-22 18:29:28 -0300

    fix: Ajusta posicionamento dos elementos dentro da HeroSection

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -12,12 +12,12 @@ export default function HeroSection() {
       >
         <div className="flex items-center">
           <div className="w-full text-white min-h-[518px]">
-            <div className="flex flex-col items-center mt-40 justify-evenly content">
+            <div className="flex flex-col items-center mt-48 justify-evenly content">
               <MotionDivLeftToRight>
                 <h1 className="w-full font-black leading-none text-center text-title2 phone1:text-title4 phone3:text-title5 tablet1:text-title6 desktop2:text-title6">
                   BEM VINDO AO MUNDO DAS PISCINAS!
                 </h1>
-                <p className="w-full mt-8 text-center phone1:w-full phone1:font-medium phone1:text-paragraph4">
+                <p className="w-full mt-12 text-center phone1:w-full phone1:font-medium phone1:text-paragraph4">
                   Aqui você será atendido por profissionais qualificados que
                   prestarão toda orientação que você precisa.
                   <br /> Continue navegando e encontre o que você precisa!
@@ -29,7 +29,7 @@ export default function HeroSection() {
                   <div className="flex justify-center w-full my-8 desktop2:mt-10">
                     <a
                       href="https://wa.me/5561992781077"
-                      className="flex flex-row items-center justify-around w-auto h-16 transition rounded-lg bg-primary hover:bg-tertiary"
+                      className="flex flex-row items-center justify-around w-auto h-16 mt-4 transition rounded-lg bg-primary hover:bg-tertiary"
                       target="_blank"
                       rel="noreferrer"
                     >
