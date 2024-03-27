4549a06abbfd33da11563ac2d2d64fc11469eb2c
Gabriel <gabrielsouza65115@gmail.com>
2024-03-27 20:40:15 -0300

    feat: Insere todos os cloros hth e genco

--- src/components/util/productsData/hth/HthProducts.jsx
+++ src/components/util/productsData/hth/HthProducts.jsx
@@ -1,6 +1,21 @@
 import hthCloros1 from "../../../../assets/imgs/products/hth/cloros/1hthCloros.jpg";
 import hthCloros2 from "../../../../assets/imgs/products/hth/cloros/2hthCloros.jpg";
 import hthCloros3 from "../../../../assets/imgs/products/hth/cloros/3hthCloros.jpg";
+import hthCloros4 from "../../../../assets/imgs/products/hth/cloros/4hthCloros.jpg";
+import hthCloros5 from "../../../../assets/imgs/products/hth/cloros/5hthCloros.jpg";
+import hthCloros6 from "../../../../assets/imgs/products/hth/cloros/6hthCloros.jpg";
+import hthCloros7 from "../../../../assets/imgs/products/hth/cloros/7hthCloros.jpg";
+import paceCloros8 from "../../../../assets/imgs/products/pace/cloros/8paceCloros.jpg";
+import paceCloros9 from "../../../../assets/imgs/products/pace/cloros/9paceCloros.jpg";
+import gencoCloros10 from "../../../../assets/imgs/products/genco/cloros/10gencoCloros.jpg";
+import gencoCloros11 from "../../../../assets/imgs/products/genco/cloros/11gencoCloros.jpg";
+import gencoCloros12 from "../../../../assets/imgs/products/genco/cloros/12gencoCloros.jpg";
+import gencoCloros13 from "../../../../assets/imgs/products/genco/cloros/13gencoCloros.jpg";
+import gencoCloros14 from "../../../../assets/imgs/products/genco/cloros/14gencoCloros.jpg";
+import gencoCloros15 from "../../../../assets/imgs/products/genco/cloros/15gencoCloros.jpg";
+import gencoCloros16 from "../../../../assets/imgs/products/genco/cloros/16gencoCloros.jpg";
+import gencoCloros17 from "../../../../assets/imgs/products/genco/cloros/17gencoCloros.jpg";
+import gencoCloros18 from "../../../../assets/imgs/products/genco/cloros/18gencoCloros.jpg";
 
 import hthAuxiliares1 from "../../../../assets/imgs/products/hth/auxiliares/1hthAuxiliares.jpg";
 import hthAuxiliares2 from "../../../../assets/imgs/products/hth/auxiliares/2hthAuxiliares.jpg";
@@ -34,6 +49,139 @@ const HthProducts = {
       description: "HTH CLORO GRANULADO ADITIVADO 10 EM 1 (SC 1,0K)",
       fispqLink:
         "https://hth.com.br/file/36673%20-%20HTH%20Cloro%20Granulado%20MB%2010%20em%201%20PT.pdf",
+      category: "cloros",
+    },
+    hthCloros4: {
+      key: 23,
+      img: hthCloros4,
+      code: 2026,
+      description: "HTH CLORO GRANULADO ADITIVADO 10 EM 1 (BD 2,5K)",
+      fispqLink:
+        "https://hth.com.br/file/36673%20-%20HTH%20Cloro%20Granulado%20MB%2010%20em%201%20PT.pdf",
+      category: "cloros",
+    },
+    hthCloros5: {
+      key: 24,
+      img: hthCloros5,
+      code: 20000,
+      description: "HTH CLORO GRANULADO TRADICIONAL 65,0% (SC 1,0K)",
+      fispqLink:
+        "https://hth.com.br/file/25931%20-%20HTH%20Cloro%20Granulado%20Trad%20PT.pdf",
+      category: "cloros",
+    },
+    hthCloros6: {
+      key: 27,
+      img: hthCloros6,
+      code: 6014,
+      description: "HTH FLUTUADOR ADVANCED PEQUENO",
+      fispqLink: "https://hth.com.br/file/26158%20-%20HTH%20flutuador%20PT.pdf",
+      category: "cloros",
+    },
+    hthCloros7: {
+      key: 28,
+      img: hthCloros7,
+      code: 6020,
+      description: "PACE FLUTUADOR ADVANCED GRANDE",
+      fispqLink: "https://hth.com.br/file/26158%20-%20HTH%20flutuador%20PT.pdf",
+      category: "cloros",
+    },
+    paceCloros8: {
+      key: 29,
+      img: paceCloros8,
+      code: 2020,
+      description: "PACE DICLORO ESTABILIZADO 60,0% (BD 10,0K)",
+      fispqLink:
+        "https://www.cloropace.com.br/wp-content/uploads/2021/02/25989-Pace-Dicloro-60_-PT.pdf",
+      category: "cloros",
+    },
+    paceCloros9: {
+      key: 30,
+      img: paceCloros9,
+      code: 2050,
+      description: "PACE DICLORO AÇÃO TOTAL (BD 10,0K)",
+      fispqLink:
+        "https://www.cloropace.com.br/wp-content/uploads/2021/02/26200-Pace-Acao-Total-PT.pdf",
+      category: "cloros",
+    },
+    gencoCloros10: {
+      key: 8,
+      img: gencoCloros10,
+      code: 2060,
+      description: "GENCO CLORO ESTABILIZADO GENCLOR (BD 10,0K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo8.pdf",
+      category: "cloros",
+    },
+    gencoCloros11: {
+      key: 9,
+      img: gencoCloros11,
+      code: 2085,
+      description: "GENCO DICLORO LINHA ESPECIAL 3 EM 1 (BD C/ 10,0K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo11.pdf",
+      category: "cloros",
+    },
+    gencoCloros12: {
+      key: 10,
+      img: gencoCloros12,
+      code: 2027,
+      description: "GENCO DICLORO LINHA ESPECIAL 3 EM 1 (BD C/ 7,5K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo11.pdf",
+      category: "cloros",
+    },
+    gencoCloros13: {
+      key: 11,
+      img: gencoCloros13,
+      code: 2081,
+      description: "GENCO DICLORO LINHA ESPECIAL 3 EM 1 (BD C/ 7,5K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo11.pdf",
+      category: "cloros",
+    },
+    gencoCloros14: {
+      key: 12,
+      img: gencoCloros14,
+      code: 2083,
+      description: "GENCO DICLORO LINHA ESPECIAL 3 EM 1 (BD C/ 2,5K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo11.pdf",
+      category: "cloros",
+    },
+    gencoCloros15: {
+      key: 13,
+      img: gencoCloros15,
+      code: 2061,
+      description: "GENCO CLORO ESTABILIZADO GENCLOR (SC 1,0K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo8.pdf",
+      category: "cloros",
+    },
+    gencoCloros16: {
+      key: 14,
+      img: gencoCloros16,
+      code: 2084,
+      description: "GENCO CLORO ESTABILIZADO GENCLOR (BD 7,5K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo8.pdf",
+      category: "cloros",
+    },
+    gencoCloros17: {
+      key: 24,
+      img: gencoCloros17,
+      code: 2041,
+      description: "GENCO CLORO GRANULADO POOL TRAT 65,0% (BD C/ 10,0K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo9.pdf",
+      category: "cloros",
+    },
+    gencoCloros18: {
+      key: 25,
+      img: gencoCloros18,
+      code: 2043,
+      description: "GENCO CLORO GRANULADO POOL TRAT 65,0% (SC C/ 1,0K)",
+      fispqLink:
+        "https://www.genco.com.br/imagens/produtos/produto-arquivo9.pdf",
       category: "cloros",
     },
   },
