e9c7f63a96a7a1dcf8dc5eed45828d9fccc84c89
Edison Matos <edison.matos@live.com>
2024-03-27 14:25:33 -0300

    feat: Arruma números

--- src/components/sections/AboutUs.jsx
+++ src/components/sections/AboutUs.jsx
@@ -25,7 +25,7 @@ export default function AboutUs() {
                 <SectionArticle
                   articleSubtitle="Nossa História"
                   articleTitle="Experiência em piscinas olímpicas e semiolímpicas"
-                  articleParagraphOne="A MPA é uma empresa familiar criada especialmente para oferecer um atendimento exclusivo e personalizado a proprietários de piscinas com grandes volumes de água, tais como piscinas olímpicas e semi olímpicas, entre outras."
+                  articleParagraphOne="A MPA é uma empresa familiar criada especialmente para oferecer um atendimento exclusivo e personalizado aos proprietários de piscinas com grandes volumes de água, tais como piscinas olímpicas e semiolímpicas, entre outras."
                   articleParagraphTwo="A sua criação remonta ao ano de 2003, mas seus idealizadores são pessoas ligadas a esse segmento há pelo menos 45 anos, dessa forma, contam com vasta experiência no atendimento diferenciado que esse tipo de negócio exige."
                   articleParagraphThree="A sua missão enquanto empresa é se aprimorar a cada dia que passa, subir degrau por degrau, avançar sempre no sentido de cumprir objetivos, entregar ao cliente exatamente o que com ele foi negociado na sua plenitude, e ao final de tudo ter a certeza de ter oferecido a ele uma boa experiência de compra. E para que tudo isso aconteça, a empresa tem a clara convicção de que precisa contar sempre com você, cliente! Junte-se à MPA Piscinas!"
                 />
