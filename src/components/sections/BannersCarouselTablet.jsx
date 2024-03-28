f4dafbb2bdb2c6d1fc29c8f1f2c366d36ec0e492
Edison Matos <edison.matos@live.com>
2024-03-28 18:16:20 -0300

    feat: Finaliza todas as animações

--- src/components/sections/BannersCarouselTablet.jsx
+++ src/components/sections/BannersCarouselTablet.jsx
@@ -1,14 +1,17 @@
 import ImagesGallery2Slides from "../interactives/ImagesGallery2Slides";
 import imgAtendimentoTablet from "../../assets/banners/bannersCarousel/atendimento-tablet.png";
 import imgEntregasTablet from "../../assets/banners/bannersCarousel/entregas-tablet.png";
+import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
 
 export default function TabletBannersCarousel() {
   return (
-    <div className="full py-[26px] phone3:py-[48px]">
-      <ImagesGallery2Slides
-        slide1={imgAtendimentoTablet}
-        slide2={imgEntregasTablet}
-      />
-    </div>
+    <MotionDivLeftToRight>
+      <div className="full py-[26px] phone3:py-[48px]">
+        <ImagesGallery2Slides
+          slide1={imgAtendimentoTablet}
+          slide2={imgEntregasTablet}
+        />
+      </div>
+    </MotionDivLeftToRight>
   );
 }
--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,14 +1,21 @@
+import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
+import MotionDivRightToLeft from "../animation/MotionDivRightToLeft";
+
 function Footer() {
   return (
     <footer className="full bg-secondary text-white font-mainFont text-center text-paragraph3 flex flex-col py-[28px] justify-between">
       <div className="content flex flex-col justify-between h-[100%] ">
-        <p className="my-[5%] tablet1:my-[3%] tablet2:my-[2%]">
-        Na sua piscina fazemos tudo, você nada!
-        </p>
-        <p className="my-[5%] tablet1:my-[3%] desktop3:my-[2%]">
-          2024 - MPA Piscinas - Todos os direitos reservados - Website
-          desenvolvido por Paper Street Softwares
-        </p>
+        <MotionDivLeftToRight>
+          <p className="my-[5%] tablet1:my-[3%] tablet2:my-[2%]">
+            Na sua piscina fazemos tudo, você nada!
+          </p>
+        </MotionDivLeftToRight>
+        <MotionDivRightToLeft>
+          <p className="my-[5%] tablet1:my-[3%] desktop3:my-[2%]">
+            2024 - MPA Piscinas - Todos os direitos reservados - Website
+            desenvolvido por Paper Street Softwares
+          </p>
+        </MotionDivRightToLeft>
       </div>
     </footer>
   );
