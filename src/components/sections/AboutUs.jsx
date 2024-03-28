0bd049c5b7650232b4eb702acfeccb601510b256
Gabriel <gabrielsouza65115@gmail.com>
2024-03-28 11:27:06 -0300

    feat: Reconstrói a seção AboutUs

--- src/components/sections/AboutUs.jsx
+++ src/components/sections/AboutUs.jsx
@@ -3,40 +3,54 @@ import ImagesGallery3Slides from "../interactives/ImagesGallery3Slides";
 import slide1 from "../../assets/banners/aboutUs/slide1bigPool.jpg";
 import slide2 from "../../assets/banners/aboutUs/slide2smallPool.jpg";
 import slide3 from "../../assets/banners/aboutUs/slide3cleanWater.jpg";
-import ArticleComponent from "../sectionElements/ArticleComponent";
-import SectionArticle from "../sectionElements/SectionArticle";
 
 export default function AboutUs() {
   return (
-
-    <div>
-      <div className="font-mainFont py-[26px] phone3:py-[48px] phone1:w-[100%]" id="about">
-        <SectionHeader
-          sectionHeaderTitleSecond="Sobre nós"
-          sectionHeaderDescription="A Mpa destaca-se no mercado pela excelência no atendimento personalizado e vasta experiência em piscinas de grandes volumes."
-        />
-
-        <div>
-          <ArticleComponent>
-            <div className=" desktop1:flex desktop1:flex-row desktop1:items-start desktop1:justify-between desktop1:gap-[0%]">
-              <div className=" desktop1:w-[60%] desktop3:w-[55%]">
-                <SectionArticle
-                  articleSubtitle="Nossa História"
-                  articleTitle="Experiência em Grandes Volumes"
-                  articleParagraphOne="A MPA é uma empresa familiar criada especialmente para oferecer um atendimento exclusivo e personalizado a proprietários de piscinas com grandes volumes de água, tais como piscinas olímpicas e semi olímpicas, entre outras."
-                  articleParagraphTwo="A sua criação remonta ao ano de 2003, mas seus idealizadores são pessoas ligadas a esse segmento há pelo menos 45 anos, dessa forma, contam com vasta experiência no atendimento diferenciado que esse tipo de negócio exige."
-                  articleParagraphThree="A sua missão enquanto empresa é se aprimorar a cada dia que passa, subir degrau por degrau, avançar sempre no sentido de cumprir objetivos, entregar ao cliente exatamente o que com ele foi negociado na sua plenitude, e ao final de tudo ter a certeza de ter oferecido a ele uma boa experiência de compra. E para que tudo isso aconteça, a empresa tem a clara convicção de que precisa contar sempre com você, cliente! Junte-se à MPA Piscinas!"
-                />
-              </div>
-              <div className=" desktop1:w-[40%] desktop1:pt-[15px]">
-                <ImagesGallery3Slides
-                  slide1={slide1}
-                  slide2={slide2}
-                  slide3={slide3}
-                />
-              </div>
+    <div
+      className="font-mainFont py-[26px] phone3:py-[48px]"
+      id="about"
+    >
+      <SectionHeader
+        sectionHeaderTitleSecond="Sobre nós"
+        sectionHeaderDescription="A Mpa destaca-se no mercado pela excelência no atendimento personalizado e vasta experiência em piscinas de grandes volumes."
+      />
+      <div>
+        <div className=" desktop1:flex desktop1:flex-row desktop1:justify-between desktop1:items-center">
+          <div className=" desktop1:w-[41.4%] desktop3:w-[46.3%]">
+            <h3 className="flex items-center phone1:w-[100%] phone1:font-medium phone1:text-paragraph3 leading-[21px] phone3:text-paragraph4 phone3:leading-6  text-quaternary"></h3>
+            <h1 className=" flex items-center phone1:w-[100%] phone1:font-medium phone1:text-title2 phone1:leading-[33px] phone3:text-title5 phone3:leading-[54px] tablet1:text-title4 tablet1:leading-[45px] text-primary">
+              Experiência em piscinas olímpicas e semiolímpicas
+            </h1>
+            <div className=" phone1:w-[100%] phone1:font-normal phone1:text-paragraph3 phone1:leading-[21px] text-quaternary desktop3:w-[84%]">
+              <p className=" desktop3:mt-[2%]">
+                A MPA é uma empresa familiar criada especialmente para oferecer
+                um atendimento exclusivo e personalizado a proprietários de
+                piscinas com grandes volumes de água, tais como piscinas
+                olímpicas e semiolímpicas, entre outras. <br /><br />A sua criação
+                remonta ao ano de 2003, mas seus idealizadores são pessoas
+                ligadas a esse segmento há pelo menos 45 anos, dessa forma,
+                contam com vasta experiência no atendimento diferenciado que
+                esse tipo de negócio exige.
+                <br /><br /> A sua missão enquanto empresa é se aprimorar a cada dia
+                que passa, subir degrau por degrau, avançar sempre no sentido de
+                cumprir objetivos, entregar ao cliente exatamente o que com ele
+                foi negociado na sua plenitude, e ao final de tudo ter a certeza
+                de ter oferecido a ele uma boa experiência de compra. E para que
+                tudo isso aconteça, a empresa tem a clara convicção de que
+                precisa contar sempre com você, cliente! Junte-se à MPA
+                Piscinas!
+              </p>
             </div>
-          </ArticleComponent>
+          </div>
+          <div className=" mt-[10%] tablet1:mt-[5%] desktop1:w-[50%] desktop3:w-[46.3%]">
+            <div className="">
+              <ImagesGallery3Slides
+                slide1={slide1}
+                slide2={slide2}
+                slide3={slide3}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/sections/AvoidAcidents.jsx
+++ src/components/sections/AvoidAcidents.jsx
@@ -1,8 +1,7 @@
 import SectionHeader from "../sectionElements/SectionHeader";
 import imgPastilhaCloro from "../../assets/imgs/avoidAccidents/pastilhaCloro.jpg";
-import resgateAcidente from "../../assets/imgs/avoidAccidents/resgateAcidente.jpg";
-import AcidentComponent from "../sectionElements/AcidentComponent";
-import ArticleAcident from "../sectionElements/ArticleAcident";
+
+
 
 export default function AboutUs() {
   return (
@@ -13,42 +12,7 @@ export default function AboutUs() {
           sectionHeaderDescription="Nesta seção, abordaremos as práticas de segurança essenciais para o armazenamento e manuseio correto de produtos para sua piscina."
         />
         <div className="">
-          <AcidentComponent>
-            <div className="desktop1:flex desktop1:flex-row-reverse desktop1:justify-between desktop1:py-[48px] ">
-              <div className="py-[28px] desktop1:w-[48%] desktop1:py-0 desktop2:w-[46%] desktop3:w-[46%]">
-                <img
-                  className=" size-[100%] desktop1:size-auto"
-                  src={imgPastilhaCloro}
-                />
-              </div>
-              <div className=" desktop1:w-[48%] desktop2:w-[46%] desktop3:w-[46.3%]">
-                <ArticleAcident
-                  articleTitle="Cuidados com o manuseio dos produtos"
-                  articleParagraphOne="Os produtos que você vai utilizar na sua piscina devem ser armazenados de forma segura, longe do alcance de crianças e em lugar seco e ventilado."
-                  articleParagraphTwo="Guarde sempre os produtos separadamente. Existem produtos que não são compatíveis uns com os outros e, se misturados, podem gerar gases nocivos à saúde e até incêndio, já que alguns são inflamáveis."
-                  articleParagraphThree="Nunca reutilize embalagens vazias, mesmo depois de serem lavadas. Elas podem acabar sendo usadas por pessoas desavisadas, crianças e até animais, e isso pode causar grande desconforto e danos à saúde."
-                />
-              </div>
-            </div>
-          </AcidentComponent>
-        </div>
-
-        <div className="">
-          <AcidentComponent>
-            <div className=" desktop1:flex desktop1:flex-row desktop1:justify-between">
-              <div className=" py-[28px] desktop1:w-[48%] desktop1:py-0 desktop2:w-[46%] deskto3:w-[47%]">
-                <img className="size-[100%] " src={resgateAcidente} />
-              </div>
-              <div className=" flex flex-col desktop1:w-[48%] desktop1:mt-[-1%] desktop2:w-[46%] desktop3:w-[46%] ">
-                <div className=" desktop3:flex desktop3:w-[100%] phone1:mt-[-5%] phone2:mt-[-4%] tablet1:mt-[-2%] desktop1:mt-[0%]">
-                  <ArticleAcident
-                    articleParagraphOne="Utilize sempre um balde plástico pequeno para fazer a dissolução do cloro ou de outro produto em pó, como barrilha, sulfato de alumínio, elevador de alcalinidade, entre outros. Esse balde deverá sempre estar guardado em lugar seguro e não poderá ser utilizado para outros fins."
-                    articleParagraphTwo="Tomando todos esses cuidados e outros relacionados à segurança da sua piscina, você garante uma diversão alegre e segura para todos os usuários, inclusive crianças e idosos. Boa diversão para todos!"
-                  />
-                </div>
-              </div>
-            </div>
-          </AcidentComponent>
+          
         </div>
       </div>
     </div>
