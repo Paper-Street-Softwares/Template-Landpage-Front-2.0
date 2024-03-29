688bd153cadad6c263580d76f0028322d6514bf8
Gabriel <adans.gabriel@gmail.com>
2024-03-29 12:28:07 -0300

    fix: Reestrutura header das páginas de produtos

--- src/components/sectionElements/HeroProducts.jsx
+++ src/components/sectionElements/HeroProducts.jsx
@@ -15,22 +15,22 @@ export default function HeroProducts(props) {
   };
 
   return (
-    <div className="z-10 bg-red-500 full font-mainFont">
+    <div className="z-10 full font-mainFont">
       <div
-        className="w-full bg-green-500 bg-center bg-cover"
+        className="w-full bg-center bg-cover"
         style={{ backgroundImage: "url(" + bgImage + ")" }}
       >
-        <div className="bg-blue-500 h-36" />
-        <div className="mx-auto bg-green-500 content">
-          <div className="flex flex-col text-center text-white bg-yellow-500 justify-evenly desktop1:gap-10 desktop1:flex-row">
+        <div className="h-36" />
+        <div className="mx-auto content">
+          <div className="flex flex-col text-center text-white mb-4 justify-evenly desktop1:gap-10 desktop1:flex-row">
             <div>
-              <h1 className="font-bold bg-green-500 text-title1 desktop1:text-start">
+              <h1 className="font-bold text-title1 desktop1:text-start">
                 <MotionDivUpToDown>{heroTitle}</MotionDivUpToDown>
               </h1>
-              <h1 className="flex justify-center w-full bg-blue-600 desktop1:justify-start">
+              <h1 className="flex justify-center w-full desktop1:justify-start">
                 <MotionDivDownToUp>
                   <img
-                    className="w-auto h-16 p-2 my-2 bg-purple-500 rounded-md max-h-16 max-w-auto"
+                    className="w-auto h-16 p-2 my-2 bg-white rounded-md max-h-16 max-w-auto"
                     src={heroImg}
                     alt="Logo HTH"
                   />
@@ -38,13 +38,12 @@ export default function HeroProducts(props) {
               </h1>
             </div>
 
-            <div className="bg-red-500">
-              <p className="bg-blue-800 text-paragraph4 tablet1:font-bold">
+            <div>
+              <p className="text-paragraph4 tablet1:font-bold">
                 <MotionDivDownToUp>{heroParagraph}</MotionDivDownToUp>
               </p>
             </div>
           </div>
-          <div className="h-4 bg-green-800" />
         </div>
       </div>
     </div>
