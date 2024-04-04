21cbbf0a0bdbee3323a7c47542eee79d7b61b4df
Gabriel <gabrielsouza65115@gmail.com>
2024-04-04 11:47:26 -0300

    feat: Insere os Produtos da Hidroazul (Cloros e Auxiliares)

---
+++ src/components/util/productsData/hidroazul/HidroazulProducts.jsx
@@ -0,0 +1,190 @@
+import hidroazulCloros1 from "../../../../assets/imgs/products/hidroazul/cloros/1hidroazulCloros.jpg";
+import hidroazulCloros2 from "../../../../assets/imgs/products/hidroazul/cloros/2hidroazulCloros.jpg";
+import hidroazulCloros3 from "../../../../assets/imgs/products/hidroazul/cloros/3hidroazulCloros.jpg";
+import hidroazulCloros4 from "../../../../assets/imgs/products/hidroazul/cloros/4hidroazulCloros.jpg";
+import hidroazulCloros5 from "../../../../assets/imgs/products/hidroazul/cloros/5hidroazulCloros.jpg";
+
+import hidroazulAuxiliares1 from "../../../../assets/imgs/products/hidroazul/auxiliares/1hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares2 from "../../../../assets/imgs/products/hidroazul/auxiliares/2hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares3 from "../../../../assets/imgs/products/hidroazul/auxiliares/3hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares4 from "../../../../assets/imgs/products/hidroazul/auxiliares/4hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares5 from "../../../../assets/imgs/products/hidroazul/auxiliares/5hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares6 from "../../../../assets/imgs/products/hidroazul/auxiliares/6hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares7 from "../../../../assets/imgs/products/hidroazul/auxiliares/7hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares8 from "../../../../assets/imgs/products/hidroazul/auxiliares/8hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares9 from "../../../../assets/imgs/products/hidroazul/auxiliares/9hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares10 from "../../../../assets/imgs/products/hidroazul/auxiliares/10hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares11 from "../../../../assets/imgs/products/hidroazul/auxiliares/11hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares12 from "../../../../assets/imgs/products/hidroazul/auxiliares/12hidroazulAuxiliares.jpg";
+import hidroazulAuxiliares13 from "../../../../assets/imgs/products/hidroazul/auxiliares/13hidroazulAuxiliares.jpg";
+
+const HidroazulProducts = {
+  cloros: {
+    hidroazulCloros1: {
+      key: 17,
+      img: hidroazulCloros1,
+      code: 2030,
+      description: "HIDROAZUL CLORO GRAN PREMIUM HIPOCLORITO 65,0% (BD 10,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/05_FDS_CLORO-GARNULADO-PREMIUM_REV00.pdf",
+      category: "cloros",
+    },
+    hidroazulCloros2: {
+      key: 18,
+      img: hidroazulCloros2,
+      code: 2099,
+      description: "HIDROAZUL CLORO GRAN PREMIUM HIPOCLORITO 65,0% (BB 40,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/05_FDS_CLORO-GARNULADO-PREMIUM_REV00.pdf",
+      category: "cloros",
+    },
+    hidroazulCloros3: {
+      key: 19,
+      img: hidroazulCloros3,
+      code: 2072,
+      description: "HIDROAZUL DICLORO FAZ! (BD 10,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/02_FDS_CLORO-FAZREV00.pdf",
+      category: "cloros",
+    },
+    hidroazulCloros4: {
+      key: 20,
+      img: hidroazulCloros4,
+      code: 2095,
+      description: "HIDROAZUL DICLORO MULTIAÇÃO (BD 10,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/01_FDS_CLORO-MULTIACAO-HIDROAZUL_REV00.pdf",
+      category: "cloros",
+    },
+    hidroazulCloros5: {
+      key: 21,
+      img: hidroazulCloros5,
+      code: 2070,
+      description: "HIDROAZUL DICLORO ORGANICO (BD 10,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/03_FDS_DICLORO-ORGANICO-HIDROAZUL_REV00.pdf",
+      category: "cloros",
+    },
+  },
+  auxiliares: {
+    hidroazulAuxiliares1: {
+      key: 12,
+      img: hidroazulAuxiliares1,
+      code: 1031,
+      description: "HIDROAZUL ALGICIDA CHOQUE (FR 1,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/24_FDS_ALGICIDA-CHOQUE-HIDROAZUL_REV01.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares2: {
+      key: 13,
+      img: hidroazulAuxiliares2,
+      code: 1030,
+      description: "HIDROAZUL ALGICIDA CHOQUE (GL 5,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/24_FDS_ALGICIDA-CHOQUE-HIDROAZUL_REV01.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares3: {
+      key: 14,
+      img: hidroazulAuxiliares3,
+      code: 1061,
+      description: "HIDROAZUL ALGICIDA MANUTENÇÃO (FR 1,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/25_FDS_ALGICIDA-MANUTENCAO-HIDROAZUL_REV00.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares4: {
+      key: 15,
+      img: hidroazulAuxiliares4,
+      code: 1065,
+      description: "HIDROAZUL ALGICIDA MANUTENÇÃO (GL 5,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/25_FDS_ALGICIDA-MANUTENCAO-HIDROAZUL_REV00.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares5: {
+      key: 16,
+      img: hidroazulAuxiliares5,
+      code: 1006,
+      description: "HIDROAZUL ALGICIDA MULTIAÇÃO (FR 1,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/26_FDS_ALGICIDA-MULTIACAO-HIDROAZUL_REV01.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares6: {
+      key: 22,
+      img: hidroazulAuxiliares6,
+      code: 5261,
+      description: "HIDROAZUL DIMINUIDOR DE PH E ALCALINIDADE (FR 1,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/22_FDS_DIMINUIDOR-DE-PH-E-ALCALINIDADE-HIDROAZUL_REV01.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares7: {
+      key: 23,
+      img: hidroazulAuxiliares7,
+      code: 4066,
+      description: "HIDROAZUL FLOC GEL DUPLO (BT C/ 2 UN)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/17_FDS_FLOC-GEL_REV01.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares8: {
+      key: 24,
+      img: hidroazulAuxiliares8,
+      code: 4067,
+      description: "HIDROAZUL FLOC PLUS 2 EM 1 (FR 1,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/18_FDS_FLOC-PLUS-2-EM-1_REV00.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares9: {
+      key: 25,
+      img: hidroazulAuxiliares9,
+      code: 4065,
+      description: "HIDROAZUL FLOC PLUS 2 EM 1 (GL 5,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/18_FDS_FLOC-PLUS-2-EM-1_REV00.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares10: {
+      key: 26,
+      img: hidroazulAuxiliares10,
+      code: 6161,
+      description: "HIDROAZUL LIMPA BORDAS (FR 1,0L)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/04/23_FDS_LIMPA-BORDAS-HIDROAZUL_REV01.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares11: {
+      key: 27,
+      img: hidroazulAuxiliares11,
+      code: 60002,
+      description: "HIDROAZUL PODEROX (BD C/ 3,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/16_FDS_PODEROX_REV00.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares12: {
+      key: 28,
+      img: hidroazulAuxiliares12,
+      code: 3021,
+      description: "HIDROAZUL WEEKEND (SC C/ 4,0K)",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/09_FDS_WEEKEND_REV00.pdf",
+      category: "auxiliares",
+    },
+    hidroazulAuxiliares13: {
+      key: 29,
+      img: hidroazulAuxiliares13,
+      code: 3020,
+      description: "HIDROAZUL TABLETE MULTIAÇÃO 200G",
+      fispqLink:
+        "https://hidroazul.com.br/wp-content/uploads/2022/12/11_FDS_PASTILHA-DE-TRICLORO-MULTIACAO_REV00.pdf",
+      category: "auxiliares",
+    },
+  },
+};
+
+export default HidroazulProducts;
--- src/pages/HidroazulPage.jsx
+++ src/pages/HidroazulPage.jsx
@@ -1,5 +1,6 @@
 import NavbarSectionProducts from "../components/sections/NavbarSectionProducts";
 import Footer from "../components/sections/Footer";
+import HidroazulProductsList from "../components/sections/HidroazulProductsList";
 import HeroProducts from "../components/sectionElements/HeroProducts";
 import BackToTopButton from "../components/interactives/BackToTopButton";
 import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
@@ -14,6 +15,7 @@ export default function HidroazulPage() {
         heroImg={imgLogo}
         heroParagraph="Aqui você encontrará tudo o que precisa para produtos  de tratamento para sua piscina."
       />
+      <HidroazulProductsList />
       <BackToTopButton />
       <FloatingWhatsappButton />
       <Footer />
