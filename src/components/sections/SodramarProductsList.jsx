63917454ad90db351e581cda145524a9e83dbef6
Gabriel <gabrielsouza65115@gmail.com>
2024-04-12 10:02:23 -0300

    fix: Efetua mudança nos cards da Netuno e apaga imports desnecessários

--- src/components/sections/SodramarProductsList.jsx
+++ src/components/sections/SodramarProductsList.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import ImgCodeDescButtonCard from "../cards/ImgCodeDescButtonCard";
 import ImgCodeDescCard from "../cards/ImgCodeDescCard";
 import SodramarProducts from "../util/productsData/sodramar/SodramarProducts";
 
