3bc2144d72dbeb119bf00de8863aa02e4348b2a7
Edison Matos <edison.matos@live.com>
2024-04-16 18:09:01 -0300

    feat: Finaliza footer pra 320px antes ainda com bgs

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,27 +1,72 @@
 import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
 import MotionDivRightToLeft from "../animation/MotionDivRightToLeft";
 import mpaLogo from "../../assets/importAssets/Logo.png";
+import { Phone, Mail } from "lucide-react";
 
 function Footer() {
   return (
-    <footer className="bg-blue-800 full gap-y-[28px]  text-white font-mainFont text-left text-paragraph3 flex flex-col py-[28px] justify-between">
-      <div className="bg-red-900 w-[88%] flex flex-col gap-y-[42px] ">
-        <div className="flex flex-col bg-yellow-600 gap-y-[16px]">
+    <footer className="bg-secondary full gap-y-[28px]  text-white font-mainFont text-left text-paragraph3 flex flex-col py-[28px] justify-between">
+      <div className="bg-blue-900 w-[88%] flex flex-col gap-y-[42px] ">
+        <div className="flex flex-col gap-y-[16px]">
           <img src={mpaLogo} className="w-[109px] h-[115px]"></img>
           <p className="full">Na sua piscinas fazemos tudo, você nada!</p>
           <div className="flex full gap-x-[8px] items-center">
-            <div className="w-[24px] h-[24px] bg-blue-600"></div>
+            <Phone size={24} strokeWidth={1} className="text-white" />
             <p>(11) 9.9488-9426</p>
           </div>
           <div className="flex full gap-x-[8px] items-center">
-            <div className="w-[24px] h-[24px] bg-blue-600"></div>
+            <Mail size={24} strokeWidth={1} className="text-white" />
             <p>contato@mpapiscinas.com.br</p>
           </div>
         </div>
-        <div className="bg-green-600">Section Sobre</div>
-        <div className="bg-blue-600">Section Navegação</div>
+        <div className="flex flex-col bg-green-800 gap-y-[16px]">
+          <h1 className="font-medium text-title2">SOBRE</h1>
+          <p>
+            Agradecemos sua visita! Esperamos que em breve você se torne nosso
+            cliente e desfrute de todos os benefícios que oferecemos.
+          </p>
+        </div>
+        <div className="flex flex-col bg-purple-800 gap-y-[16px]">
+          <h1 className="font-medium text-title2">NAVEGAÇÃO</h1>
+          <div className="flex justify-between bg-green-800 full">
+            <div className="bg-red-800 w-[46%] flex flex-col gap-y-[16px] text-paragraph3">
+              <a href="#" className="font-semibold h-[36px] ">
+                Início
+              </a>
+              <a href="#" className="font-semibold h-[36px]">
+                Sobre nós
+              </a>
+              <a href="#" className="font-semibold h-[36px]">
+                Produtos e Acessórios
+              </a>
+            </div>
+            <div className="bg-red-800 w-[46%] flex flex-col gap-y-[16px] text-paragraph3">
+              <a href="#" className="font-semibold h-[36px] ">
+                Curso e Palestras
+              </a>
+              <a href="#" className="font-semibold h-[36px]">
+                Perguntas Frequentes
+              </a>
+              <a href="#" className="font-semibold h-[36px]">
+                Contato
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="bg-red-900 w-[88%]">Section Créditos</div>
+      <div className="bg-red-900 w-[88%]">
+        <p>2024 - MPA Piscinas - Todos os direitos reservados</p>
+        <p>
+          Gostaria de ter um site como este?{" "}
+          <a
+            className="underline"
+            target="_blank"
+            href="https://www.paperstreet.com.br"
+          >
+            Clique aqui
+          </a>
+        </p>
+      </div>
     </footer>
   );
 }
