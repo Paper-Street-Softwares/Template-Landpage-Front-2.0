c0ad18d9120a20e207b4af1924cfd969a8084667
Edison Matos <edison.matos@live.com>
2024-04-17 14:15:11 -0300

    fix: Ajusta banners sem content

--- src/components/sections/BannersCarouselDesktop.jsx
+++ src/components/sections/BannersCarouselDesktop.jsx
@@ -6,11 +6,13 @@ import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
 export default function BannersCarousel() {
   return (
     <MotionDivLeftToRight>
-      <div className="full py-[26px] phone3:py-[48px]">
-        <ImagesGallery2Slides
-          slide1={imgEntregasDesktop}
-          slide2={imgAtendimentoDesktop}
-        />
+      <div className="full py-[26px] phone3:py-[48px] flex justify-center">
+        <div className="w-[88%] max-w-[1110px]">
+          <ImagesGallery2Slides
+            slide1={imgEntregasDesktop}
+            slide2={imgAtendimentoDesktop}
+          />
+        </div>
       </div>
     </MotionDivLeftToRight>
   );
