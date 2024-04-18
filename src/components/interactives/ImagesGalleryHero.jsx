3734b64ec00e86339954f50c921c4b5d55f6227f
Edison Matos <edison.matos@live.com>
2024-04-18 16:59:34 -0300

    feat: Finaliza hero versão desktop

---
+++ src/components/interactives/ImagesGalleryHero.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import PropTypes from "prop-types";
+import "react-image-gallery/styles/css/image-gallery.css";
+
+export default function ImagesGalleryHero(props) {
+  const { slide1, slide2, slide3 } = props;
+
+  ImagesGalleryHero.propTypes = {
+    slide1: PropTypes.any,
+    slide2: PropTypes.any,
+    slide3: PropTypes.any,
+  };
+
+  const images = [
+    {
+      original: slide1,
+      thumbnail: slide1,
+      originalAlt: "Produto para limpeza de piscina",
+    },
+    {
+      original: slide2,
+      thumbnail: slide2,
+      originalAlt: "Produto para tratamento de água de piscina",
+    },
+    {
+      original: slide3,
+      thumbnail: slide3,
+      originalAlt: "Cloros para tratamento de piscinas",
+    },
+  ];
+
+  return (
+    <div>
+      <ImageGallery
+        items={images}
+        showBullets={false}
+        autoPlay={true}
+        showFullscreenButton={false}
+        useBrowserFullscreen={false}
+        showPlayButton={false}
+        showThumbnails={false}
+        showNav={false}
+      />
+    </div>
+  );
+}
