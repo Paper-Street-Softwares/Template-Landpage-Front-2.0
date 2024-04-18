d995478ce2a1df4114838e3d7c6780909e4a1b45
Edison Matos <edison.matos@live.com>
2024-04-18 10:15:12 -0300

    feat: Update BannersCarouselDesktop.jsx

--- src/components/sections/BannersCarouselDesktop.jsx
+++ src/components/sections/BannersCarouselDesktop.jsx
@@ -6,11 +6,13 @@ import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
 export default function BannersCarousel() {
   return (
     <MotionDivLeftToRight>
-      <div className="full py-[26px] phone3:py-[48px]">
+      <div className="full py-[26px] phone3:py-[48px] flex justify-center">
+        <div className="w-[88%] max-w-[1110px]">
         <ImagesGallery2Slides
           slide1={imgAtendimentoDesktop}
           slide2={imgEntregasDesktop}
         />
+        </div>
       </div>
     </MotionDivLeftToRight>
   );
