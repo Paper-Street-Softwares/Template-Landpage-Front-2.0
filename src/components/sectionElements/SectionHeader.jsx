6eee7e744d6face3083acb82c7444b5fd6ab8c38
Gabriel <gabrielsouza65115@gmail.com>
2024-05-06 14:43:47 -0300

    fix: Corrige as tags do Componente Section Header

--- src/components/sectionElements/SectionHeader.jsx
+++ src/components/sectionElements/SectionHeader.jsx
@@ -16,14 +16,14 @@ export default function SectionHeader(props) {
   return (
     <div className="flex flex-col items-center mb-[28px] desktop1:mb-[48px]">
       <div className="w-[79%] flex flex-col justify-center font-mainFont text-title1 phone1:text-title2 phone3:text-title5 text-center font-medium">
-        <MotionDivLeftToRight>{sectionHeaderTitleFirst}</MotionDivLeftToRight>
+        <MotionDivLeftToRight><h1>{sectionHeaderTitleFirst}</h1></MotionDivLeftToRight>
       </div>
       <div className="w-[79%] flex flex-col justify-center font-mainFont text-primary text-title1 phone1:text-title2 phone3:text-title5 text-center font-medium ">
-        <MotionDivRightToLeft>{sectionHeaderTitleSecond}</MotionDivRightToLeft>
+        <MotionDivRightToLeft><h1>{sectionHeaderTitleSecond}</h1></MotionDivRightToLeft>
       </div>
       <div className="w-full h-px mt-6 mb-6 bg-neutral-400"></div>
       <div className="w-full tablet1:w-[79%] text-quaternary font-mainFont text-paragraph3 phone3:text-paragraph4 text-center">
-        <MotionDivLeftToRight>{sectionHeaderDescription}</MotionDivLeftToRight>
+        <MotionDivLeftToRight><p>{sectionHeaderDescription}</p></MotionDivLeftToRight>
       </div>
     </div>
   );
