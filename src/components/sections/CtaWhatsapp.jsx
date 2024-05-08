76c9d75ea183981464cffb86a060b8aa41651816
Gabriel <gabrielsouza65115@gmail.com>
2024-05-08 10:31:49 -0300

    fix: Corrige dimensões da imagem da pessoa na Sessão CtaWhatsapp

--- src/components/sections/CtaWhatsapp.jsx
+++ src/components/sections/CtaWhatsapp.jsx
@@ -27,7 +27,9 @@ export default function CtaWhatsapp() {
             <div className="flex flex-col w-full  justify-evenly items-left h-28 phone1:h-40 phone3:h-48 desktop1:h-52 desktop1:mb-[24px]">
               <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 phone3:max-w-none font-poppins tablet1:text-title2 tablet2:text-title4 desktop1:text-title5 desktop2:text-title6">
                 <MotionDivUpToDown>
-                  <p>Contate-nos através do <strong>Whatsapp</strong></p>
+                  <p>
+                    Contate-nos através do <strong>Whatsapp</strong>
+                  </p>
                 </MotionDivUpToDown>
               </h1>
               <MotionDivDownToUp>
@@ -50,7 +52,7 @@ export default function CtaWhatsapp() {
                 </div>
               </MotionDivDownToUp>
             </div>
-            <div className=" h-50 w-[90%] max-w-40 phone1:w-[100%] phone1:max-w-none phone3:max-w-60 tablet1:w-[45%]">
+            <div className=" phone1:min-h-[197px] phone1:max-h-[229.39px] phone2:min-h-[229.40px] phone2:max-h-[261px] phone3:min-h-[262px] phone3:max-h-[334px] tablet1:min-h-[235px] tablet1:max-h-[292px] tablet2:min-h-[292px] tablet2:max-h-[335px] desktop1:min-h-[335px] desktop3:max-h-[335px] w-[90%] max-w-40 phone1:w-[100%] phone1:max-w-none phone3:max-w-60 tablet1:w-[45%]">
               <MotionDivRightToLeft>
                 <img alt="Foto Pessoa" src={imgCta} />
               </MotionDivRightToLeft>
