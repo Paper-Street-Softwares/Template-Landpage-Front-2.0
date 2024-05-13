798a9165eb8f4871ed7f4e865a2c288d282e96fd
Edison Matos <edison.matos@live.com>
2024-05-13 16:47:09 -0300

    feat: Limpa arquivos em main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ImportPage from "./pages/ImportPage.jsx";
 import HthPage from "./pages/HthPage.jsx";
-import GencoPage from "./pages/GencoPage.jsx";
-import DomclorPage from "./pages/DomclorPage.jsx";
-import HidroazulPage from "./pages/HidroazulPage.jsx";
-import NeoclorPage from "./pages/NeoclorPage.jsx";
-import NetunoPage from "./pages/NetunoPage.jsx";
-import SodramarPage from "./pages/SodramarPage.jsx";
-import SuallPage from "./pages/SuallPage.jsx";
 import IndexRita from "./pages/rita/IndexRita.jsx";
 import "./index.css";
 
