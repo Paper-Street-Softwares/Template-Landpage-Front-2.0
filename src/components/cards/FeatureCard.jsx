1167b94b21f904f2704f95a9e800472efe2a84a0
Edison Matos <edison.matos@live.com>
2024-05-15 16:28:13 -0300

    feat: Insere svg como propriedade do button

--- src/components/cards/FeatureCard.jsx
+++ src/components/cards/FeatureCard.jsx
@@ -1,4 +1,4 @@
-import ButtonWithIcone from "../interactives/ButtonWithIcon";
+import ButtonWithIcon from "../interactives/ButtonWithIcon";
 
 export default function FeatureCard(props) {
   const { img, title, description } = props;
@@ -17,7 +17,26 @@ export default function FeatureCard(props) {
         <p className="font-secondFont text-paragraph4 mb-[36px] text-quaternary">
           {description}
         </p>
-        <ButtonWithIcone label="Saiba mais" />
+        <ButtonWithIcon
+          label="Saiba mais"
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-move-right"
+            >
+              <path d="M18 8L22 12L18 16" />
+              <path d="M2 12H22" />
+            </svg>
+          }
+        />
       </div>
     </div>
   );
