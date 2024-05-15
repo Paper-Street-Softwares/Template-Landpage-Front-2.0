09b3920e93f1fa2d0096d39409f3c1477e365634
Edison Matos <edison.matos@live.com>
2024-05-15 16:19:19 -0300

    feat: Torna itens do card em componentes

--- src/components/cards/FeatureCard.jsx
+++ src/components/cards/FeatureCard.jsx
@@ -1,21 +1,21 @@
 import ButtonWithIcone from "../interactives/ButtonWithIcon";
-import bgImgCard from "../../assets/imgs/hero/bgHero.webp";
 
-export default function FeatureCard() {
+export default function FeatureCard(props) {
+  const { img, title, description } = props;
+
   return (
     <div className="bg-white p-[36px] rounded-2xl desktop1:w-[30%] flex flex-col tablet1:flex-row gap-[36px] desktop1:flex-col w-full">
       <img
-        src={bgImgCard}
+        src={img}
         alt=""
         className="tablet1:w-[50%] desktop1:w-full h-auto rounded-2xl"
       />
       <div className="">
         <h1 className="font-mainFont text-[32px] leading-[36px] mb-[16px]">
-          Aquisição e Locação
+          {title}
         </h1>
         <p className="font-secondFont text-paragraph4 mb-[36px] text-quaternary">
-          Nosso seguro automotivo abrangente garante sua proteção e
-          tranquilidade em cada viagem.
+          {description}
         </p>
         <ButtonWithIcone label="Saiba mais" />
       </div>
--- src/components/sections/Featuresv2.jsx
+++ src/components/sections/Featuresv2.jsx
@@ -2,6 +2,7 @@ import SectionArea from "../sectionElements/SectionArea";
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionWrapper from "../sectionElements/SectionWrapper";
 import FeatureCard from "../cards/FeatureCard";
+import imgCard from "../../assets/imgs/paralaxe/bgDarkHouse.webp";
 
 export default function Featuresv2() {
   return (
@@ -13,9 +14,21 @@ export default function Featuresv2() {
       />
       <SectionWrapper>
         <div className="flex flex-wrap justify-between w-full gap-[36px] tablet1:gap-[24px] desktop1:flex-row">
-          <FeatureCard />
-          <FeatureCard />
-          <FeatureCard />
+          <FeatureCard
+            img={imgCard}
+            title="Venda e Aluguel"
+            description="Conte com nossa experiência para vender ou alugar seu imóvel com rapidez e segurança, garantindo a melhor oferta do mercado."
+          />
+          <FeatureCard
+            img={imgCard}
+            title="Avaliação"
+            description="Realizamos avaliações precisas que captam o verdadeiro valor do seu imóvel, preparando-o para uma oferta ideal."
+          />
+          <FeatureCard
+            img={imgCard}
+            title="Negociação e Documentação"
+            description="Facilitamos todo o processo de documentação e conduzimos negociações eficazes para assegurar uma transação tranquila e vantajosa."
+          />
         </div>
       </SectionWrapper>
     </SectionArea>
