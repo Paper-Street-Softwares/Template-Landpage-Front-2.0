5f7aa355db46a78c5605e8ebf4284008d0e8911c
Edison Matos <edison.matos@live.com>
2024-05-16 16:21:01 -0300

    feat: Insere className no motionDivDownToUp

--- src/components/animation/MotionDivDownToUp.jsx
+++ src/components/animation/MotionDivDownToUp.jsx
@@ -3,7 +3,7 @@ import { useInView } from "react-intersection-observer";
 import { Children } from "react";
 import PropTypes from "prop-types";
 
-export default function MotionDivDownToUp({ children }) {
+export default function MotionDivDownToUp({ children, className }) {
   const { ref, inView } = useInView({ triggerOnce: true });
   const childrenArray = Children.toArray(children);
 
@@ -14,9 +14,10 @@ export default function MotionDivDownToUp({ children }) {
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 200 }}
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: inView ? 1 : 0, y: inView ? 0 : 150 }}
       transition={{ duration: 1 }}
+      className={`${className}`}
     >
       {childrenArray}
     </motion.div>
