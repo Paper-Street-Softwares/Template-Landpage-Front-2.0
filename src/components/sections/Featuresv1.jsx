ec6979151f45806262c170e9e23133a481ad67a6
Edison Matos <edison.matos@live.com>
2024-05-16 09:44:16 -0300

    fix: Remove temporariamente chamamento de card featureBgImgCard

--- src/components/sections/Featuresv1.jsx
+++ src/components/sections/Featuresv1.jsx
@@ -1,11 +1,6 @@
 import SectionArea from "../sectionElements/SectionArea";
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionWrapper from "../sectionElements/SectionWrapper";
-import FeatureBgImgCard from "../cards/FeatureBgImgCard";
-import imgCasas from "../../assets/imgs/hero/bgHero.webp";
-import imgAps from "../../assets/imgs/hero/bgHero.webp";
-import imgTerrenos from "../../assets/imgs/hero/bgHero.webp";
-import imgPontos from "../../assets/imgs/hero/bgHero.webp";
 
 export default function Featuresv1() {
   return (
@@ -16,28 +11,7 @@ export default function Featuresv1() {
         sectionHeaderSubtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at"
       />
       <SectionWrapper>
-        <div className="flex flex-wrap justify-between w-full gap-[36px] tablet1:gap-[24px]">
-          <FeatureBgImgCard
-            bgImg={imgCasas}
-            title="Casas"
-            description="Descubra a casa dos seus sonhos! Ambientes sofisticados e confortáveis esperam por você em locais desejados. Venha encontrar seu novo lar!"
-          />
-          <FeatureBgImgCard
-            bgImg={imgAps}
-            title="Apartamentos"
-            description="Elegância e praticidade em nossos apartamentos sofisticados. Ideal para quem busca um estilo de vida moderno e conveniente"
-          />
-          <FeatureBgImgCard
-            bgImg={imgTerrenos}
-            title="Terrenos"
-            description="Construa a realidade que você deseja em nossos terrenos bem localizados. A oportunidade perfeita para desenvolver seu projeto ideal."
-          />
-          <FeatureBgImgCard
-            bgImg={imgPontos}
-            title="Pontos Comerciais"
-            description="Posicione seu negócio para o sucesso! Nossos pontos comerciais estão nos melhores locais, maximizando sua visibilidade e acesso."
-          />
-        </div>
+        <div className="flex flex-wrap justify-between w-full gap-[36px] tablet1:gap-[24px]"></div>
       </SectionWrapper>
     </SectionArea>
   );
