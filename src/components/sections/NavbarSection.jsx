213bdc59381a80ee42bce1c697a2f3581dca224a
Gabriel <adans.gabriel@gmail.com>
2024-05-17 15:54:30 -0300

    feat: Adiciona botão de contato WhatsApp ao lado da Sidebar em telas responsivas

--- src/components/sections/NavbarSection.jsx
+++ src/components/sections/NavbarSection.jsx
@@ -6,6 +6,7 @@ import Sidebar from "../sectionElements/Sidebar";
 import { Link as ScrollLink } from "react-scroll";
 import HeadlessDemo from "../sectionElements/Sidebar2.0";
 import { px } from "framer-motion";
+import ButtonWithIcon from "../interactives/ButtonWithIcon";
 
 export default function NavbarSection() {
   const [scrolling, setScrolling] = useState(false);
@@ -86,7 +87,7 @@ export default function NavbarSection() {
       <div
         className={`fixed z-20 w-full transition-colors duration-1000  ${
           scrolling
-            ? 'bg-white bg-opacity-100 shadow-lg -mt-[75px] transition-all duration-1000'
+            ? 'bg-white bg-opacity-100 shadow-lg tablet1:-mt-[80px] -mt-[100px] transition-all duration-1000'
             : 'transition-all duration-1000'
         }`}
       >
@@ -102,7 +103,7 @@ export default function NavbarSection() {
           >
             <img
               src={Logo}
-              alt="Logo MPA Piscinas"
+              alt="Logo Rita Almeida Corretora"
               className={`py-[8px] ${
                 scrolling
                   ? "h-[40px] max-h-[40px] transition-all duration-1000 bg-black"
@@ -110,7 +111,24 @@ export default function NavbarSection() {
               } tablet3:mb-0`}
             />
           </ScrollLink>
-          <div className="flex bg-green-500 desktop1:hidden">SIDEBAR AQUI</div>
+          <div className="flex items-center justify-between gap-[16px]">
+            <div className="hidden tablet1:flex desktop1:hidden">
+              <ButtonWithIcon
+              label="Contato"
+              icon={<svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="text-white bi bi-whatsapp"
+                viewBox="0 0 16 16"
+              >
+                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+              </svg>}
+            />
+            </div>
+            <div className="flex items-center bg-green-500 desktop1:hidden">SIDEBAR AQUI</div>
+          </div>
           {showListGroup ? <ListGroup /> : null}
         </Navbar>
         <div className="flex "></div>
