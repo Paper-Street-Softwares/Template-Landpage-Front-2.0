28131b7069bd3cc76e4c25ac1b109f4c9794d025
Edison Matos <edison.matos@live.com>
2024-05-21 14:51:54 -0300

    feat: Ajusta items da listgroup

--- src/components/interactives/ButtonWithIconNavbar.jsx
+++ src/components/interactives/ButtonWithIconNavbar.jsx
@@ -2,10 +2,10 @@ export default function ButtonWithIcon(props, className) {
   const { icon, label } = props;
 
   return (
-    <button className="flex flex-row items-center justify-around transition rounded-[40px] px-[18px] py-[10px] bg-black hover:bg-white text-white hover:text-black hover:border-black hover:border-solid border-[1px] border-black">
+    <button className="flex flex-row items-center justify-center transition rounded-[40px] px-[18px] py-[10px] bg-black hover:bg-white text-white hover:text-black hover:border-black hover:border-solid border-[1px] border-black">
       <div className="flex items-center text-center gap-[10px]">
-        <div className="">{icon}</div>
-        <p className="flex items-cente text-paragraph3 font-mainFont font-medium">
+        <div className="flex flex-col justify-end">{icon}</div>
+        <p className="flex items-center text-paragraph3 font-secondFont font-bold">
           {label}
         </p>
       </div>
