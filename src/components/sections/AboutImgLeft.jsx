350fc60e5426d17ad89a10ac58d6b9e60bbc9369
Gabriel <gabrielsouza65115@gmail.com>
2024-05-21 11:37:26 -0300

    feat: Insere o Modal no botão da Sessão AboutImgLeft já com o texto da Sessão estilizado

--- src/components/sections/AboutImgLeft.jsx
+++ src/components/sections/AboutImgLeft.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Paragraphs from "../sectionElements/Paragraphs";
 import SectionArea from "../sectionElements/SectionArea";
 import SectionTitles from "../sectionElements/SectionTitles";
@@ -5,8 +6,50 @@ import SectionWrapper from "../sectionElements/SectionWrapper";
 import imgAboutPerson from "../../assets/imgs/about/aboutPerson.jpg";
 import ButtonWithIcon from "../interactives/ButtonWithIcon";
 import MotionDivDownToUp from "../animation/MotionDivDownToUp";
+import { Dialog } from "primereact/dialog";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 
 export default function AboutImgLeft() {
+  const [visible, setVisible] = useState(false);
+  const [modalContent, setModalContent] = useState("");
+  const [modalTitle, setModalTitle] = useState("");
+
+  const onClick = () => {
+    setModalTitle("Sobre mim");
+    setModalContent(
+      <p>
+        Creci: 11798/PE<br></br> Ensino Superior: Administração - Facape<br></br><br></br> Olá, me chamo
+        Rita Almeida, sou corretora de imóveis há mais de 10 anos e sou formada
+        em Administração há 30. Decidi entrar no ramo quando um colega me fez um
+        elogio: "Rita, você é tão comunicativa, deveria ser corretora de
+        imóveis." Eu ainda fiquei receosa: "Ah, isso não é para mim," mas decidi
+        arriscar. Fiz o curso quando minha filha ainda era pequena e não me
+        arrependo da decisão que tomei.<br></br><br></br> Com a minha formação em Administração,
+        percebi rapidamente que muitas das habilidades adquiridas durante o
+        curso eram essenciais no mercado imobiliário. A capacidade de negociar,
+        organizar finanças e entender os fundamentos econômicos são apenas
+        algumas das competências que me ajudaram a destacar-me na profissão.<br></br><br></br> Nos
+        primeiros anos, equilibrar a vida pessoal com a profissional foi um
+        desafio, especialmente sendo mãe de uma criança pequena. Contudo, sempre
+        busquei manter um alto padrão de ética e dedicação, o que me permitiu
+        construir uma reputação sólida e uma base de clientes fiéis.<br></br><br></br> Ao longo
+        dos anos, vi muitas mudanças no setor imobiliário. A tecnologia
+        transformou a maneira como interagimos com clientes e como os imóveis
+        são apresentados e comercializados. Adaptar-me a essas mudanças foi
+        essencial, e sempre procurei estar atualizada, seja através de cursos de
+        formação continuada ou participando de seminários e workshops.<br></br><br></br> Hoje,
+        posso dizer que minha carreira é uma grande paixão. Cada dia traz um
+        novo desafio e a oportunidade de ajudar alguém a encontrar o lugar ideal
+        para chamar de lar. Continuo empregando as habilidades de administração
+        no meu dia a dia, gerenciando não apenas propriedades, mas também sonhos
+        e expectativas.
+      </p>
+    );
+    setVisible(true);
+  };
+
   return (
     <SectionArea className="">
       <SectionWrapper className="flex flex-col desktop1:flex-row gap-[40px] desktop1:gap-0 desktop1:justify-between">
@@ -39,6 +82,7 @@ export default function AboutImgLeft() {
           <MotionDivDownToUp>
             <ButtonWithIcon
               label="Leia mais"
+              onClick={onClick}
               icon={
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -46,7 +90,7 @@ export default function AboutImgLeft() {
                   height="24"
                   viewBox="0 0 24 24"
                   fill="none"
-                  stroke="currentcolor"
+                  stroke="currentColor"
                   strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
@@ -60,6 +104,15 @@ export default function AboutImgLeft() {
           </MotionDivDownToUp>
         </div>
       </SectionWrapper>
+      <Dialog
+        header={modalTitle}
+        visible={visible}
+        onHide={() => setVisible(false)}
+        style={{ width: "50vw" }}
+        breakpoints={{ "960px": "75vw", "641px": "90vw" }}
+      >
+        <p className="m-0 font-secondFont">{modalContent}</p>
+      </Dialog>
     </SectionArea>
   );
 }
