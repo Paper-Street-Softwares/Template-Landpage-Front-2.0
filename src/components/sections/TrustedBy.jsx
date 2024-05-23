7585de98fe252635a1dd22e3610e7b58ad6aa47e
Gabriel <gabrielsouza65115@gmail.com>
2024-05-23 11:17:27 -0300

    feat: Insere logos de teste para estilizar posições e espaçamentos para as logos reais

--- src/components/sections/TrustedBy.jsx
+++ src/components/sections/TrustedBy.jsx
@@ -15,65 +15,67 @@ export default function TrustedBy() {
         <MotionDivDownToUp className="w-full">
           <div className="flex flex-wrap justify-between w-full gap-[20px] tablet1:gap-[24px]">
             <img
-              src=""
+              src="src\assets\imgs\logos\image 28.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\image 29.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src/assets/imgs/logos/image 30.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
+            />
+
+            <img
+              src="src\assets\imgs\logos\image 31.png"
+              alt=""
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\image 32.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\image 33.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
+            />
+
+            {/* <img
+              src="src\assets\imgs\logos\images 3.png"
+              alt=""
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\download 3.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\download 4.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\download 3.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className=" max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\download 4.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
+              className=" max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
             />
             <img
-              src=""
+              src="src\assets\imgs\logos\download 4.png"
               alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
-            />
-            <img
-              src=""
-              alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
-            />
-            <img
-              src=""
-              alt=""
-              className="bg-gray-300 w-[45%] min-h-[30px] max-h-[50px] tablet1:w-[30%] desktop1:w-[13%]"
-            />
+              className=" w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] max-h-[40px] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%] desktop3:max-h-[120px]"
+            /> */}
           </div>
         </MotionDivDownToUp>
       </SectionWrapper>
