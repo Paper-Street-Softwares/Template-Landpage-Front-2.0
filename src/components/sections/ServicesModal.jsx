dfce073d74090fb00dbfb664cff0438f8dac45a7
Gabriel <gabrielsouza65115@gmail.com>
2024-06-03 11:13:10 -0300

    feat: Insere o botão dentro do CardModal

--- src/components/sections/ServicesModal.jsx
+++ src/components/sections/ServicesModal.jsx
@@ -58,17 +58,33 @@ const ServicesModal = () => {
                           alt=""
                         />
                       </div>
-                      <div className="flex">
+                      <div className="flex flex-col gap-[32px]">
                         <div className="flex flex-col gap-[12px]">
-                          <h2 className="flex font-bold">Pra quem é esse seguro?</h2>
+                          <h2 className="flex font-bold">
+                            Pra quem é esse seguro?
+                          </h2>
                           <p className="flex">
                             It is a long established fact that a reader will be
                             distracted by the readable content of a page when
                             looking at its layout. 
                           </p>
                         </div>
-                        <div className="w-full">
-                          <button></button>
+                        <div className="">
+                          <ButtonWithIcon
+                            label="Simular Agora"
+                            icon={
+                              <svg
+                                width="24"
+                                height="24"
+                                viewBox="0 0 1024 1024"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="#FFFFFF"
+                              >
+                                <path d="M893.8 130.2c-58.8-58.8-154.5-58.8-213.3 0l-453 453c-4.3 4.3-7.4 9.6-9.1 15.4L136.5 873.2c-5.3 17.9-0.8 37.5 12.1 50.4 12.9 12.9 32.5 17.4 50.4 12.1l274.5-81.8c5.8-1.7 11.1-4.8 15.4-9.1l453-453c58.8-58.8 58.8-154.5 0-213.3zM378 791.4l-169.4 50.5 50.5-169.4 378.3-378.3 118.8 118.8L378 791.4zM803.1 366.3L684.3 247.5l75.1-75.1c19.5-19.5 51.1-19.5 70.6 0l48.2 48.2c19.5 19.5 19.5 51.1 0 70.6L803.1 366.3z" />
+                                <path d="M165.3 877.9l-.6.4-28 8.3 8.3-28.1.4-.6zM1024 960v64H0v-64z" />
+                              </svg>
+                            }
+                          />
                         </div>
                       </div>
                     </div>
