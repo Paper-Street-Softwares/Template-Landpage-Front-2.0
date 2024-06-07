50519c64c0b68edc4f5f4ee890319d86642b6531
Edison Matos <edison.matos@live.com>
2024-06-07 10:59:07 -0300

    feat: Atualiza paralaxes

--- src/abstractions/content.jsx
+++ src/abstractions/content.jsx
@@ -6,12 +6,12 @@ import trustedByImg3 from "../assets/imgs/trustedBy/item3.webp";
 import trustedByImg4 from "../assets/imgs/trustedBy/item4.webp";
 import trustedByImg5 from "../assets/imgs/trustedBy/item5.webp";
 import trustedByImg6 from "../assets/imgs/trustedBy/item6.webp";
-import numbersImgBg from "../assets/imgs/paralaxe/bgParalaxe1.webp";
+import numbersImgBg from "../assets/imgs/paralaxe/bgParalaxe1.jpg";
 import featuresImg1 from "../assets/imgs/features/featuresImg1.webp";
 import featuresImg2 from "../assets/imgs/features/featuresImg2.webp";
 import featuresImg3 from "../assets/imgs/features/featuresImg3.webp";
 import featuresImg4 from "../assets/imgs/features/featuresImg4.webp";
-import ctaWhatsappImgBg from "../assets/imgs/paralaxe/bgParalaxe2.webp";
+import ctaWhatsappImgBg from "../assets/imgs/paralaxe/bgParalaxe2.jpg";
 
 const content = {
   texts: {
