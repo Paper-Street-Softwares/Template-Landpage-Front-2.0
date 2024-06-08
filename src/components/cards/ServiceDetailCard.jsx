5f55f31f8a4fa654c4647661df7f72a426463c3b
Edison Matos <edison.matos@live.com>
2024-06-08 07:37:25 -0300

    feat: Cria ServiceDetailCard

---
+++ src/components/cards/ServiceDetailCard.jsx
@@ -0,0 +1,30 @@
+export default function ServiceDetailCard(props) {
+  const { img, subtitle, description, buttonLabel, buttonIcon, buttonLink } =
+    props;
+
+  return (
+    <div className="w-full flex flex-col gap-[16px]">
+      <div className="w-full">
+        <div
+          style={{
+            backgroundImage: `url(${img})`,
+          }}
+          className="w-[100%] h-[180px] phone2:h-[230px] phone3:h-[250px] desktop1:h-[300px] bg-no-repeat bg-top bg-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-[32px]">
+        <div className="flex flex-col gap-[12px]">
+          <h2 className="font-bold text-title1 text-black">{subtitle}</h2>
+          <p>{description}</p>
+        </div>
+        <div className="">
+          <ButtonWithIcon
+            label={buttonLabel}
+            icon={buttonIcon}
+            link={buttonLink}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
