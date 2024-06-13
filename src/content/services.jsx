cdac90e68cbb819552c77ac3e89816520ebc4b18
Edison Matos <edison.matos@live.com>
2024-06-13 18:43:41 -0300

    feat: Finaliza cards e links

--- src/content/services.jsx
+++ src/content/services.jsx
@@ -47,6 +47,10 @@ import morePatrimonialServicesImg39 from "../assets/imgs/cardModal/imgCardModal3
 import morePatrimonialServicesImg40 from "../assets/imgs/cardModal/imgCardModal40.png";
 import morePatrimonialServicesImg41 from "../assets/imgs/cardModal/imgCardModal41.png";
 
+import links from "./links";
+
+const whatsappContactLink = `https://wa.me/` + `${links.ctaWhatsapp}`;
+
 const services = {
   main: {
     card1: {
@@ -54,39 +58,50 @@ const services = {
       title: "Seguro Automóvel",
       description:
         "Proteção completa para seu veículo, com assistência 24 horas e serviços personalizados.",
-      buttonLink: "",
+      buttonLabel: "Simular",
+      buttonLink:
+        "https://villa.segfy.com:443/Publico/Segurados/Orcamentos/SolicitarCotacao?e=V%2BfN%2BEC0GstumVAM%2B2hIUA%3D%3D",
     },
     card2: {
       img: { img: mainServicesImg2, alt: "" },
-      title: "Seguro Residencial e Condomínio",
+      title: "Seguro Residencial",
       description:
         "Coberturas abrangentes para sua casa ou apartamento, incluindo danos estruturais e responsabilidade civil.",
-      buttonLink: "",
+      buttonLabel: "Simular",
+      buttonLink:
+        "https://villa.segfy.com:443/Publico/Segurados/Orcamentos/SolicitarCotacaoResidencial?e=V%2BfN%2BEC0GstumVAM%2B2hIUA%3D%3D",
     },
     card3: {
       img: { img: mainServicesImg3, alt: "" },
-      title: "Seguro de Vida e Acidentes",
+      title: "Seguro Condomínio",
       description:
-        "Segurança financeira para sua família em casos de imprevistos, com coberturas adaptadas às suas necessidades.",
-      buttonLink: "",
+        "Seguro Condomínio protege seu prédio contra danos e responsabilidades, garantindo tranquilidade para todos os moradores.",
+      buttonLabel: "Simular",
+      buttonLink:
+        "https://villa.segfy.com:443/Publico/Segurados/Orcamentos/SolicitarCotacaoCondominio?e=V%2BfN%2BEC0GstumVAM%2B2hIUA%3D%3D",
     },
     card4: {
       img: { img: mainServicesImg4, alt: "" },
-      title: "Seguro Empresarial",
+      title: "Seguro de Vida e Acidentes Pessoais",
       description:
         "Segurança financeira para sua família em casos de imprevistos, com coberturas adaptadas às suas necessidades.",
-      buttonLink: "",
+      buttonLabel: "Simular",
+      buttonLink:
+        "https://villa.segfy.com:443/Publico/Segurados/Orcamentos/SolicitarCotacaoVida?e=V%2BfN%2BEC0GstumVAM%2B2hIUA%3D%3D",
     },
     card5: {
       img: { img: mainServicesImg5, alt: "" },
-      title: "Planos de Saúde",
+      title: "Seguro Empresarial",
       description:
-        "Segurança financeira para sua família em casos de imprevistos, com coberturas adaptadas às suas necessidades.",
-      buttonLink: "",
+        "O seguro empresarial protege seu negócio contra imprevistos, garantindo segurança e continuidade operacional.",
+      buttonLabel: "Simular",
+      buttonLink:
+        "https://villa.segfy.com:443/Publico/Segurados/Orcamentos/SolicitarCotacaoEmpresarial?e=V%2BfN%2BEC0GstumVAM%2B2hIUA%3D%3D",
     },
     card6: {
       title: "Demais seguros",
       description: "Confira nossa lista completa de seguros clicando abaixo.",
+      buttonLabel: "Simular",
       buttonLink: "",
     },
   },
@@ -109,17 +124,22 @@ const services = {
       </svg>
     ),
     ctaButtons: {
-      buttonLabel: "Simular",
+      buttonLabel: "Contratar",
       icon: (
         <svg
-          width="24"
-          height="24"
-          viewBox="0 0 1024 1024"
           xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
+          width="15"
+          height="15"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-corner-down-right"
         >
-          <path d="M893.8 130.2c-58.8-58.8-154.5-58.8-213.3 0l-453 453c-4.3 4.3-7.4 9.6-9.1 15.4L136.5 873.2c-5.3 17.9-0.8 37.5 12.1 50.4 12.9 12.9 32.5 17.4 50.4 12.1l274.5-81.8c5.8-1.7 11.1-4.8 15.4-9.1l453-453c58.8-58.8 58.8-154.5 0-213.3zM378 791.4l-169.4 50.5 50.5-169.4 378.3-378.3 118.8 118.8L378 791.4zM803.1 366.3L684.3 247.5l75.1-75.1c19.5-19.5 51.1-19.5 70.6 0l48.2 48.2c19.5 19.5 19.5 51.1 0 70.6L803.1 366.3z" />
-          <path d="M165.3 877.9l-.6.4-28 8.3 8.3-28.1.4-.6zM1024 960v64H0v-64z" />
+          <polyline points="15 10 20 15 15 20" />
+          <path d="M4 4v7a4 4 0 0 0 4 4h12" />
         </svg>
       ),
     },
@@ -132,7 +152,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Ideal para viajantes que querem transcender fronteiras com segurança, desfrutando de cada destino sem preocupações.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service2: {
         name: "Seguro Auto",
@@ -142,7 +162,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Recomendado para motoristas que desejam proteger seu veículo, embarcando em suas jornadas com segurança e confiança.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service3: {
         name: "Seguro Vida On",
@@ -152,7 +172,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para indivíduos que desejam garantir o futuro de seus entes queridos, tecendo um legado de cuidado e proteção.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service4: {
         name: "Seguro Fiança",
@@ -162,7 +182,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Ideal para inquilinos e proprietários que buscam segurança nos contratos de locação, tecendo uma relação de confiança.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service5: {
         name: "Seguro Auto Jovem",
@@ -172,7 +192,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Destinado a jovens motoristas que buscam proteção e segurança ao iniciarem sua jornada no trânsito.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service6: {
         name: "Seguro de Acidentes Pessoais Individual Prazo Curto",
@@ -182,7 +202,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem busca proteção em situações específicas e temporárias, garantindo segurança em momentos cruciais.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service7: {
         name: "Seguro de Acidentes Pessoais Escolar",
@@ -192,7 +212,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para escolas que desejam garantir a segurança de seus alunos, protegendo-os em suas atividades diárias.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service8: {
         name: "Previdência para Crianças",
@@ -202,7 +222,7 @@ const services = {
         cardSubtitle: "Pra quem esta previdência é indicada?",
         cardDescription:
           "Para pais que querem assegurar o futuro financeiro de seus filhos, entrelaçando segurança e prosperidade.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service9: {
         name: "Seguro Vida",
@@ -212,7 +232,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para indivíduos que querem garantir a segurança financeira de seus entes queridos, criando um legado de cuidado.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service10: {
         name: "Seguro Auto Mulher",
@@ -222,7 +242,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Destinado a mulheres motoristas que desejam proteção especial, navegando com segurança pelo trânsito diário.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service11: {
         name: "Seguro de Vida em Grupo",
@@ -232,7 +252,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para empresas que desejam proteger seus funcionários, orquestrando uma rede de segurança coletiva.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service12: {
         name: "Seguro Saúde",
@@ -242,7 +262,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem busca proteção abrangente para sua saúde, promovendo bem-estar e tranquilidade.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service13: {
         name: "Consórcio",
@@ -252,7 +272,7 @@ const services = {
         cardSubtitle: "Pra quem o consórcio é indicado?",
         cardDescription:
           "Para quem deseja adquirir bens de forma planejada, orquestrando um futuro seguro e próspero.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service14: {
         name: "Previdência",
@@ -262,7 +282,7 @@ const services = {
         cardSubtitle: "Pra quem a previdência é indicada?",
         cardDescription:
           "Para quem planeja o futuro financeiro, garantindo tranquilidade e segurança nos anos vindouros.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service15: {
         name: "Seguro Odontológico",
@@ -272,7 +292,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem deseja cuidar da saúde bucal, garantindo sorrisos saudáveis e seguros.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service16: {
         name: "Seguro de Vida Mais Mulher",
@@ -282,7 +302,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Destinado a mulheres que desejam uma proteção diferenciada, entrelaçando cuidado e segurança.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service17: {
         name: "Seguro de Vida Individual",
@@ -292,7 +312,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem deseja proteção personalizada, garantindo segurança financeira aos entes queridos.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service18: {
         name: "Seguro Auto Sênior",
@@ -302,7 +322,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para motoristas seniores que querem garantir segurança e tranquilidade ao dirigir, protegendo-se no trânsito.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service19: {
         name: "Seguro de Acidentes Pessoais Plus",
@@ -312,7 +332,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem deseja uma proteção ampla contra acidentes, garantindo segurança em várias situações.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
     },
     empresarial: {
@@ -324,7 +344,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para empresas que desejam proteger sua frota de veículos, garantindo segurança e tranquilidade nas operações.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service2: {
         name: "Seguro de Vida Empresarial",
@@ -334,7 +354,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para empresários que desejam proteger seus sócios e funcionários, tecendo uma rede de segurança corporativa.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service3: {
         name: "Seguro Imobiliária",
@@ -344,7 +364,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para imobiliárias que desejam proteger seus negócios, garantindo uma gestão tranquila e segura dos imóveis.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service4: {
         name: "Seguro Saúde Ocupacional",
@@ -354,7 +374,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para empresas que querem assegurar a saúde de seus funcionários, promovendo um ambiente de trabalho seguro e saudável.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service5: {
         name: "Seguro para Convenções Coletivas",
@@ -364,7 +384,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Ideal para sindicatos ou organizações que precisam se proteger de mudanças proveniente de decisões em grupo.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service6: {
         name: "Seguro Condomínio",
@@ -374,7 +394,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para síndicos e moradores que desejam proteger áreas comuns e privativas, promovendo um ambiente seguro.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
     },
     patrimonial: {
@@ -386,7 +406,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para famílias que desejam proteger seu lar contra imprevistos, garantindo segurança e tranquilidade no mosaico de suas vidas.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service2: {
         name: "Seguro Moto",
@@ -396,7 +416,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para motociclistas que querem proteger sua paixão sobre duas rodas, navegando o labirinto urbano com tranquilidade.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service3: {
         name: "Seguro Equipamentos Portáteis",
@@ -406,7 +426,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Perfeito para quem carrega tecnologia valiosa no dia a dia, orquestrando proteção em sua rotina digital.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service4: {
         name: "Seguro para notebooks",
@@ -416,7 +436,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para profissionais e estudantes que dependem de seus notebooks, garantindo segurança em seu trabalho e estudos.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service5: {
         name: "Seguro para Câmera Fotográfica",
@@ -426,7 +446,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para fotógrafos que desejam proteger seus equipamentos, capturando momentos sem preocupações.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service6: {
         name: "Seguro Residência Veraneio",
@@ -436,7 +456,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem possui casa de veraneio e quer desfrutar de momentos tranquilos, protegendo seu refúgio particular.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service7: {
         name: "Seguro Táxi",
@@ -446,7 +466,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para taxistas que querem garantir a segurança de seu veículo, navegando a cidade com tranquilidade.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service8: {
         name: "Seguro Caminhão",
@@ -456,7 +476,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para caminhoneiros que desejam proteger seu meio de vida, enfrentando as estradas com segurança.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service9: {
         name: "Seguro Residência Habitual",
@@ -466,7 +486,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para famílias que querem proteger seu lar cotidiano, tecendo uma rede de segurança e tranquilidade.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service10: {
         name: "Seguro para Filmadora",
@@ -476,7 +496,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para videomakers que buscam proteger suas filmadoras, capturando suas histórias com segurança.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service11: {
         name: "Seguro Residência Apartamento",
@@ -486,7 +506,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para moradores de apartamentos que desejam proteger seu lar, garantindo segurança em cada detalhe do cotidiano.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service12: {
         name: "Seguro para Celular",
@@ -496,7 +516,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem deseja proteger seu celular, transcendendo as preocupações e aproveitando a tecnologia sem medos.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service13: {
         name: "Consórcio de Imóvel",
@@ -506,7 +526,7 @@ const services = {
         cardSubtitle: "Pra quem o consórcio é indicado?",
         cardDescription:
           "Para quem deseja adquirir um imóvel, entrelaçando economia e planejamento na realização de sonhos.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service14: {
         name: "Seguro Residência Premium",
@@ -516,7 +536,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem busca proteção completa e sofisticada para seu lar, transcendendo as preocupações cotidianas.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service15: {
         name: "Seguro Auto Premium",
@@ -526,7 +546,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para motoristas que buscam uma proteção superior, garantindo segurança e tranquilidade nas suas viagens.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
       service16: {
         name: "Seguro para Tablet",
@@ -536,7 +556,7 @@ const services = {
         cardSubtitle: "Pra quem é esse seguro?",
         cardDescription:
           "Para quem depende de seu tablet no dia a dia, garantindo proteção e tranquilidade digital.",
-        buttonLink: "#",
+        buttonLink: whatsappContactLink,
       },
     },
   },
