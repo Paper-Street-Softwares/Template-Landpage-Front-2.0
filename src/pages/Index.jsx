b2d1642873980ce3081a801c183b6401d3688295
Edison Matos <edison.matos@live.com>
2024-06-14 15:37:46 -0300

    feat: Deixa página apenas com os elementos devidos

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -10,7 +10,6 @@ import CtaWhatsapp from "../components/sections/CtaWhatsapp";
 import NavbarSection from "../components/sections/NavbarSection";
 import BackToTopButton from "../components/interactives/BackToTopButton";
 import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
-import CtaParalaxeLeft from "../components/sections/CtaParalaxeLeft";
 
 export default function Index() {
   return (
@@ -20,7 +19,6 @@ export default function Index() {
       <TrustedBy />
       <Numbers />
       <About />
-      <CtaParalaxeLeft />
       <Features />
       <Services />
       <CtaWhatsapp />
