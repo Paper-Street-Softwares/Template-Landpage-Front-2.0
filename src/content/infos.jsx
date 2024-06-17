91e7c974656bc0ee0023c26b5cc098878b0ba2fa
Edison Matos <edison.matos@live.com>
2024-06-17 11:49:13 -0300

    feat: Finaliza footer

--- src/content/infos.jsx
+++ src/content/infos.jsx
@@ -4,6 +4,13 @@ const infos = {
   phone: "(71) 98179-5510",
   mail: "moniquea@harmonic.com",
   footerInfos: {
+    expediente: (
+      <p>
+        Segunda-feira à Sexta-feira
+        <br />
+        Das 8h às 12h - 14h às 18h
+      </p>
+    ),
     year: 2024,
     footerText:
       "Agradecemos sua visita! Esperamos em breve poder recebê-lo como nosso paciente, proporcionando a reconstrução do seu sorriso e elevando sua autoestima!",
--- src/content/links.jsx
+++ src/content/links.jsx
@@ -4,8 +4,8 @@ const links = {
     "+5571981795510?text=Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.",
   ctaLink: "#",
   socialMedia: {
-    instagram: "#",
-    facebook: "#",
+    instagram: "https://www.instagram.com/moniqueadrielle",
+    facebook: "https://www.facebook.com/monique.adrielle.14/",
     linkedin: "#",
     twitter: "#",
   },
