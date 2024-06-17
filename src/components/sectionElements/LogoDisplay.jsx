65ade8b49be647ce06a21ac46127e74f9f289e81
Edison Matos <edison.matos@live.com>
2024-06-17 16:36:26 -0300

    feat: Finaliza trustedBy

--- src/components/sectionElements/LogoDisplay.jsx
+++ src/components/sectionElements/LogoDisplay.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const LogoDisplay = ({ images }) => {
   return (
-    <div className="flex flex-wrap justify-between w-full gap-y-[60px] p-[20px] phone2:p-[40px] desktop1:py-0 desktop1:px-[80px]">
+    <div className="flex flex-wrap justify-evenly desktop1:justify-between w-full gap-y-[60px] p-[20px] phone2:p-[40px] desktop1:py-0 desktop1:px-[80px]">
       {images.map((image, index) => (
         <img
           key={index}
           src={image.src}
           alt={image.alt}
-          className="max-h-[40px] w-[45.3%] phone3:max-h-[50px] phone3:w-[39.3%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:p-[20px] desktop1:w-[28.3%] desktop1:max-h-[80px] desktop3:w-[29.7%]"
+          className="max-h-[50px] w-[80%] object-contain tablet1:w-[26.1%] tablet2:max-h-[60px] desktop1:p-[20px] desktop1:w-[28.3%] desktop1:max-h-[100px] desktop3:w-[33%]"
         />
       ))}
     </div>
--- src/components/sections/TrustedBy.jsx
+++ src/components/sections/TrustedBy.jsx
@@ -2,7 +2,6 @@ import SectionArea from "../sectionElements/SectionArea";
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionWrapper from "../sectionElements/SectionWrapper";
 import MotionDivDownToUp from "../animation/MotionDivDownToUp";
-import Paragraphs from "../sectionElements/Paragraphs";
 import LogoDisplay from "../sectionElements/LogoDisplay";
 import content from "../../content/content";
 import { Dialog } from "primereact/dialog";
@@ -25,18 +24,6 @@ const images = [
     src: content.texts.trustedBy.images.img3.img.trustedByImg3,
     alt: content.texts.trustedBy.images.img3.alt,
   },
-  // {
-  //   src: content.texts.trustedBy.images.img4.img.trustedByImg4,
-  //   alt: content.texts.trustedBy.images.img4.alt,
-  // },
-  // {
-  //   src: content.texts.trustedBy.images.img5.img.trustedByImg5,
-  //   alt: content.texts.trustedBy.images.img5.alt,
-  // },
-  // {
-  //   src: content.texts.trustedBy.images.img6.img.trustedByImg6,
-  //   alt: content.texts.trustedBy.images.img6.alt,
-  // },
 ];
 
 export default function TrustedBy() {
@@ -55,14 +42,14 @@ export default function TrustedBy() {
   };
 
   return (
-    <SectionArea className="bg-quinary">
+    <SectionArea className="">
       <SectionHeader
         className="text-center"
         sectionHeaderTitle={content.texts.trustedBy.title}
         sectionHeaderSubtitle={content.texts.trustedBy.subtitle}
       />
       <SectionWrapper>
-        <MotionDivDownToUp className="w-full mb-[26px] tablet1:mb-[40px] desktop1:mb-[80px]">
+        <MotionDivDownToUp className="w-full">
           <LogoDisplay images={images} />
         </MotionDivDownToUp>
         <MotionDivDownToUp className="">
