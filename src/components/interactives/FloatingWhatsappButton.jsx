98f24782338800bc62239cfd86cec049e3371248
Edison Matos <edison.matos@live.com>
2024-06-18 08:46:34 -0300

    feat: Remove formulário do whatsappFlotingButton

--- src/components/interactives/FloatingWhatsappButton.jsx
+++ src/components/interactives/FloatingWhatsappButton.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import whatsappWebm from "../../assets/importAssets/whatsappGif.gif";
 import Modal from "../util/Modal";
+import links from "../../content/links";
+
+const whatsappContactLink = `https://wa.me/` + `${links.ctaWhatsapp}`;
 
 const FloatingWhatsappButton = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -26,16 +29,18 @@ const FloatingWhatsappButton = () => {
       className={`${
         isVisible ? "block animate-fade-in" : "hidden"
       } fixed bottom-2 right-3 p-4 text-secondary z-20 rounded-full focus:outline-none transition`}
-      onClick={() => setOpenModal(!openModal)}
+      // onClick={() => setOpenModal(!openModal)}
     >
       {openModal ? null : (
-        <button>
-          <img
-            className="w-16"
-            src={whatsappWebm}
-            alt="Floating WhatsApp Button"
-          />
-        </button>
+        <a href={whatsappContactLink} target="_blank">
+          <button>
+            <img
+              className="w-16"
+              src={whatsappWebm}
+              alt="Floating WhatsApp Button"
+            />
+          </button>
+        </a>
       )}
       <Modal isOpen={openModal} setCloseModal={setOpenModal} />
     </button>
