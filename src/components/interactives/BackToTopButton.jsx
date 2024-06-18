bff64770cd73fe21706848ea9056b3d482b4e4b3
Edison Matos <edison.matos@live.com>
2024-06-18 08:40:44 -0300

    fix: Corrige alternância de cores em backToTop

--- src/components/interactives/BackToTopButton.jsx
+++ src/components/interactives/BackToTopButton.jsx
@@ -30,7 +30,7 @@ const BackToTopButton = () => {
     <button
       className={`${
         isVisible ? "block animate-fade-in" : "hidden"
-      } fixed bottom-8 left-8 p-4 bg-primary text-secondary rounded-full z-10 focus:outline-none hover:bg-darker transition`}
+      } fixed bottom-8 left-8 p-4 bg-primary border-primary border-solid border-[1px] text-secondary rounded-full z-10 focus:outline-none hover:border-secondary hover:invert transition`}
       onClick={scrollToTop}
     >
       <FaArrowUp />
